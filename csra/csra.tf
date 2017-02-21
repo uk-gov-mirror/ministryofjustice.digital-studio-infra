2fe54bb69cd813ec4432f1c8e37f4d0ea4258e00
Glen Mailer <glenjamin@gmail.com>
2017-02-21 11:38:03 +0000

    Basic template for CSRA webapp, no config yet

--- csra/csra.tf
+++ csra/csra.tf
@@ -44,3 +44,12 @@ resource "azurerm_sql_database" "csra-poc" {
     }
 }
 
+resource "azurerm_template_deployment" "csra-poc-webapp" {
+  name = "csra-poc-webapp"
+  resource_group_name = "${azurerm_resource_group.csra-poc.name}"
+  deployment_mode = "Incremental"
+  template_body = "${file("./webapp.template.json")}"
+  parameters {
+    environment = "PoC"
+  }
+}
