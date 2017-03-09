219c0e343ac6c4aa7449c64810b2a2009c075bca
Glen Mailer <glenjamin@gmail.com>
2017-03-09 13:51:26 +0000

    Prep for using on production too

---
+++ azure-prod.tf
@@ -0,0 +1,7 @@
+provider "azurerm" {
+  # NOMS Digital Studio Production 1
+  subscription_id = "a5ddf257-3b21-4ba9-a28c-ab30f751b383"
+  # client_id = "..." use ARM_CLIENT_ID env var
+  # client_secret = "..." use ARM_CLIENT_SECRET env var
+  tenant_id = "747381f4-e81f-4a43-bf68-ced6a1e14edf"
+}
