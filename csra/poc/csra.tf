b758293f59505e1bfbd4a15312064b3ea53296a4
Glen Mailer <glenjamin@gmail.com>
2017-03-10 10:36:26 +0000

    Add noms.dsd.io DNS to CSRA, and create production skeleton

--- csra/poc/csra.tf
+++ csra/poc/csra.tf
@@ -13,6 +13,33 @@ resource "azurerm_template_deployment" "csra-poc-webapp" {
   deployment_mode = "Incremental"
   template_body = "${file("../webapp.template.json")}"
   parameters {
+    hostname = "${azurerm_dns_cname_record.csra-poc.name}.${azurerm_dns_cname_record.csra-poc.zone_name}"
     environment = "PoC"
   }
+}
+
+resource "azurerm_dns_cname_record" "csra-poc" {
+    name = "csra-poc"
+    zone_name = "noms.dsd.io"
+    resource_group_name = "webops"
+    ttl = "300"
+    record = "csra-poc.azurewebsites.net"
+    tags {
+        Service = "CSRA"
+        Environment = "PoC"
+    }
+}
+
+# The "production" site currently uses this non-production DNS entry
+# which can only be configured via the non-prod subscription
+resource "azurerm_dns_cname_record" "csra-prod" {
+    name = "csra"
+    zone_name = "noms.dsd.io"
+    resource_group_name = "webops"
+    ttl = "300"
+    record = "csra-prod.azurewebsites.net"
+    tags {
+        Service = "CSRA"
+        Environment = "Prod"
+    }
 }
