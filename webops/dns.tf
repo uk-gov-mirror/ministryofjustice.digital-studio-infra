7e1b9ea1df6577077c4611a597a7f6da6baca3a2
Glen Mailer <glenjamin@gmail.com>
2017-03-16 11:55:58 +0000

    Add HMPPS dsd.io DNS zone

--- webops/dns.tf
+++ webops/dns.tf
@@ -1,12 +1,25 @@
 resource "azurerm_dns_zone" "noms" {
-   name = "noms.dsd.io"
-   resource_group_name = "${azurerm_resource_group.webops.name}"
+    name = "noms.dsd.io"
+    resource_group_name = "${azurerm_resource_group.webops.name}"
+    tags {
+        Service = "WebOps"
+        Environment = "Management"
+    }
+}
+
+resource "azurerm_dns_zone" "hmpps" {
+    name = "hmpps.dsd.io"
+    resource_group_name = "${azurerm_resource_group.webops.name}"
+    tags {
+        Service = "WebOps"
+        Environment = "Management"
+    }
 }
 
 resource "azurerm_dns_cname_record" "search" {
-   name = "search"
-   zone_name = "${azurerm_dns_zone.noms.name}"
-   resource_group_name = "${azurerm_resource_group.webops.name}"
-   ttl = "300"
-   record = "search-noms-api.dsd.io"
+    name = "search"
+    zone_name = "${azurerm_dns_zone.noms.name}"
+    resource_group_name = "${azurerm_resource_group.webops.name}"
+    ttl = "300"
+    record = "search-noms-api.dsd.io"
 }
