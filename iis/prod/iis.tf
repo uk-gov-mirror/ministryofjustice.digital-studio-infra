55f55d36ee70d3b81d25e8a4f2608ca1eb67f167
Glen Mailer <glenjamin@gmail.com>
2017-03-16 14:49:42 +0000

    Finish converting to new terraform remote storage

--- iis/prod/iis.tf
+++ iis/prod/iis.tf
@@ -1,3 +1,15 @@
+terraform {
+    required_version = ">= 0.9.0"
+    backend "azure" {
+        resource_group_name = "webops-prod"
+        storage_account_name = "nomsstudiowebopsprod"
+        container_name = "terraform"
+        key = "iis-prod.terraform.tfstate"
+        arm_subscription_id = "a5ddf257-3b21-4ba9-a28c-ab30f751b383"
+        arm_tenant_id = "747381f4-e81f-4a43-bf68-ced6a1e14edf"
+    }
+}
+
 resource "azurerm_resource_group" "iis-prod" {
     name = "iis-prod"
     location = "ukwest"
