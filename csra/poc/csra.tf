dc98e49207a047ff692dfa458ea2476253daa265
Glen Mailer <glenjamin@gmail.com>
2017-03-16 14:44:22 +0000

    More upgrading to 0.9 state storage

--- csra/poc/csra.tf
+++ csra/poc/csra.tf
@@ -1,3 +1,16 @@
+terraform {
+    required_version = ">= 0.9.0"
+    backend "azure" {
+        resource_group_name = "webops"
+        storage_account_name = "nomsstudiowebops"
+        container_name = "terraform"
+        key = "csra-poc.terraform.tfstate"
+        arm_subscription_id = "c27cfedb-f5e9-45e6-9642-0fad1a5c94e7"
+        arm_tenant_id = "747381f4-e81f-4a43-bf68-ced6a1e14edf"
+    }
+}
+
+
 resource "azurerm_resource_group" "csra-poc" {
     name = "csra-poc"
     location = "ukwest"
---
+++ webops/prod/state-storage.tf
@@ -0,0 +1,20 @@
+resource "azurerm_storage_account" "webops" {
+  name = "nomsstudiowebopsprod"
+  resource_group_name = "${azurerm_resource_group.webops.name}"
+  location = "${azurerm_resource_group.webops.location}"
+  account_kind = "BlobStorage"
+  account_type = "Standard_GRS"
+  access_tier = "Hot"
+  enable_blob_encryption = true
+  tags {
+    Service = "WebOps"
+    Environment = "Management"
+  }
+}
+
+resource "azurerm_storage_container" "terraform" {
+  name = "terraform"
+  resource_group_name = "${azurerm_resource_group.webops.name}"
+  storage_account_name = "${azurerm_storage_account.webops.name}"
+  container_access_type = "private"
+}
