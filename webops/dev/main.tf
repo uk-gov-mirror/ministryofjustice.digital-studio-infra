40dd047a1ca9f44af39dda4c3826132b64f6284e
Glen Mailer <glenjamin@gmail.com>
2017-03-16 14:44:22 +0000

    More upgrading to 0.9 state storage

--- webops/dev/main.tf
+++ webops/dev/main.tf
@@ -4,7 +4,7 @@ terraform {
         resource_group_name = "webops"
         storage_account_name = "nomsstudiowebops"
         container_name = "terraform"
-        key = "webops.terraform.tfstate"
+        key = "webops-dev.terraform.tfstate"
         arm_subscription_id = "c27cfedb-f5e9-45e6-9642-0fad1a5c94e7"
         arm_tenant_id = "747381f4-e81f-4a43-bf68-ced6a1e14edf"
     }
