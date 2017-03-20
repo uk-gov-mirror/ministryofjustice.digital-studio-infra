8336552bfe503bfab1bb5dd37949f5f11edd8735
Glen Mailer <glenjamin@gmail.com>
2017-03-20 15:42:09 +0000

    Create 'production' DNS zone

---
+++ webops/prod/dns.tf
@@ -0,0 +1,12 @@
+resource "azurerm_dns_zone" "service-hmpps" {
+    name = "service.hmpps.dsd.io"
+    resource_group_name = "${azurerm_resource_group.webops.name}"
+    tags {
+        Service = "WebOps"
+        Environment = "Management"
+    }
+}
+
+output "service.hmpps.dsd.io nameservers" {
+    value = ["${azurerm_dns_zone.service-hmpps.name_servers}"]
+}
