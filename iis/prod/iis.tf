0e4f35dcdd9c78c9fb96d81b1db624c136c06f49
Glen Mailer <glenjamin@gmail.com>
2017-03-21 17:40:49 +0000

    Sync up iis production template with staging

--- iis/prod/iis.tf
+++ iis/prod/iis.tf
@@ -10,84 +10,105 @@ terraform {
     }
 }
 
-resource "azurerm_resource_group" "iis-prod" {
-    name = "iis-prod"
-    location = "ukwest"
-    tags {
-      Service = "IIS"
-      Environment = "Prod"
-    }
+variable "app-name" {
+    type = "string"
+    default = "iis-prod"
 }
-
-resource "random_id" "iis-prod-sql-admin-password" {
-    byte_length = 16
-}
-resource "random_id" "iis-prod-sql-user-password" {
-    byte_length = 16
-}
-
-resource "azurerm_sql_server" "iis-prod" {
-    name = "iis-prod"
-    resource_group_name = "${azurerm_resource_group.iis-prod.name}"
-    location = "${azurerm_resource_group.iis-prod.location}"
-    version = "12.0"
-    administrator_login = "iis"
-    administrator_login_password = "${random_id.iis-prod-sql-admin-password.b64}"
-    tags {
+variable "tags" {
+    type = "map"
+    default {
         Service = "IIS"
         Environment = "Prod"
     }
 }
 
-resource "azurerm_sql_firewall_rule" "iis-prod-office-access" {
+resource "azurerm_resource_group" "group" {
+    name = "${var.app-name}"
+    location = "ukwest"
+    tags = "${var.tags}"
+}
+
+resource "random_id" "sql-admin-password" {
+    byte_length = 16
+}
+resource "random_id" "sql-user-password" {
+    byte_length = 16
+}
+
+resource "azurerm_sql_server" "sql" {
+    name = "${var.app-name}"
+    resource_group_name = "${azurerm_resource_group.group.name}"
+    location = "${azurerm_resource_group.group.location}"
+    version = "12.0"
+    administrator_login = "iis"
+    administrator_login_password = "${random_id.sql-admin-password.b64}"
+    tags = "${var.tags}"
+}
+
+resource "azurerm_sql_firewall_rule" "office-access" {
     name = "NOMS Studio office"
-    resource_group_name = "${azurerm_resource_group.iis-prod.name}"
-    server_name = "${azurerm_sql_server.iis-prod.name}"
+    resource_group_name = "${azurerm_resource_group.group.name}"
+    server_name = "${azurerm_sql_server.sql.name}"
     start_ip_address = "${var.ips["office"]}"
     end_ip_address = "${var.ips["office"]}"
 }
 
-resource "azurerm_sql_database" "iis-prod" {
-    name = "iis-prod"
-    resource_group_name = "${azurerm_resource_group.iis-prod.name}"
-    location = "${azurerm_resource_group.iis-prod.location}"
-    server_name = "${azurerm_sql_server.iis-prod.name}"
+resource "azurerm_sql_firewall_rule" "app-access" {
+    count = "${length(split(",", azurerm_template_deployment.webapp.outputs.ips))}"
+    name = "Application IP ${count.index}"
+    resource_group_name = "${azurerm_resource_group.group.name}"
+    server_name = "${azurerm_sql_server.sql.name}"
+    start_ip_address = "${element(split(",", azurerm_template_deployment.webapp.outputs.ips), count.index)}"
+    end_ip_address = "${element(split(",", azurerm_template_deployment.webapp.outputs.ips), count.index)}"
+}
+
+resource "azurerm_sql_database" "db" {
+    name = "${var.app-name}"
+    resource_group_name = "${azurerm_resource_group.group.name}"
+    location = "${azurerm_resource_group.group.location}"
+    server_name = "${azurerm_sql_server.sql.name}"
     edition = "Standard"
     requested_service_objective_name = "S3"
-    tags {
-        Service = "IIS"
-        Environment = "Prod"
-    }
+    tags = "${var.tags}"
 }
 
 resource "azurerm_template_deployment" "sql-tde" {
     name = "sql-tde"
-    resource_group_name = "${azurerm_resource_group.iis-prod.name}"
+    resource_group_name = "${azurerm_resource_group.group.name}"
     deployment_mode = "Incremental"
     template_body = "${file("../../shared/azure-sql-tde.template.json")}"
     parameters {
-        serverName = "${azurerm_sql_server.iis-prod.name}"
-        databaseName = "${azurerm_sql_database.iis-prod.name}"
-        service = "IIS"
-        environment = "Prod"
+        serverName = "${azurerm_sql_server.sql.name}"
+        databaseName = "${azurerm_sql_database.db.name}"
+        service = "${var.tags["Service"]}"
+        environment = "${var.tags["Environment"]}"
     }
 }
 
-resource "azurerm_template_deployment" "iis-prod-webapp" {
-    name = "iis-prod"
-    resource_group_name = "${azurerm_resource_group.iis-prod.name}"
+resource "azurerm_template_deployment" "webapp" {
+    name = "${var.app-name}"
+    resource_group_name = "${azurerm_resource_group.group.name}"
     deployment_mode = "Incremental"
     template_body = "${file("../webapp.template.json")}"
     parameters {
-        name = "iis-prod"
-        hostname = "hpa.service.hmpps.dsd.io"
-        service = "IIS"
-        environment = "Prod"
+        name = "${var.app-name}"
+        hostname = "${azurerm_dns_cname_record.cname.name}.${azurerm_dns_cname_record.cname.zone_name}"
+        service = "${var.tags["Service"]}"
+        environment = "${var.tags["Environment"]}"
         DB_USER = "iis-user"
-        DB_PASS = "${random_id.iis-prod-sql-user-password.b64}"
-        DB_SERVER = "${azurerm_sql_server.iis-prod.fully_qualified_domain_name}"
-        DB_NAME = "${azurerm_sql_database.iis-prod.name}"
+        DB_PASS = "${random_id.sql-user-password.b64}"
+        DB_SERVER = "${azurerm_sql_server.sql.fully_qualified_domain_name}"
+        DB_NAME = "${azurerm_sql_database.db.name}"
     }
+}
+
+resource "azurerm_dns_cname_record" "cname" {
+    name = "hpa"
+    zone_name = "service.hmpps.dsd.io"
+    resource_group_name = "webops-prod"
+    ttl = "300"
+    record = "${var.app-name}.azurewebsites.net"
+    tags = "${var.tags}"
 }
 
 output "advice" {
