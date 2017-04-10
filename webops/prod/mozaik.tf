9133abd30e3b87a1e2ef5bacaee7e7d97b9ff4d1
Glen Mailer <glenjamin@gmail.com>
2017-04-10 17:35:57 +0100

    Create overwatch dashboard, including github sync

---
+++ webops/prod/mozaik.tf
@@ -0,0 +1,71 @@
+variable "mozaik_tags" {
+    type = "map"
+    default {
+        Service = "WebOps"
+        Environment = "Prod"
+    }
+}
+
+resource "azurerm_template_deployment" "mozaik" {
+    name = "mozaik"
+    resource_group_name = "${azurerm_resource_group.webops.name}"
+    deployment_mode = "Incremental"
+    template_body = "${file("../../shared/appservice.template.json")}"
+    parameters {
+        name = "overwatch-mozaik"
+        service = "${var.mozaik_tags["Service"]}"
+        environment = "${var.mozaik_tags["Environment"]}"
+    }
+}
+
+resource "azurerm_dns_cname_record" "mozaik-cname" {
+    name = "overwatch-dash"
+    zone_name = "service.hmpps.dsd.io"
+    resource_group_name = "${azurerm_resource_group.webops.name}"
+    ttl = "300"
+    record = "${azurerm_template_deployment.mozaik.parameters.name}.azurewebsites.net"
+    tags = "${var.mozaik_tags}"
+}
+
+resource "azurerm_template_deployment" "mozaik-hostname" {
+    name = "mozaik-hostname"
+    resource_group_name = "${azurerm_resource_group.webops.name}"
+    deployment_mode = "Incremental"
+    template_body = "${file("../../shared/appservice-hostname.template.json")}"
+
+    parameters {
+        name = "${azurerm_template_deployment.mozaik.parameters.name}"
+        hostname = "${azurerm_dns_cname_record.mozaik-cname.name}.${azurerm_dns_cname_record.mozaik-cname.zone_name}"
+    }
+
+    depends_on = ["azurerm_template_deployment.mozaik"]
+}
+
+resource "azurerm_template_deployment" "mozaik-github" {
+    name = "mozaik-github"
+    resource_group_name = "${azurerm_resource_group.webops.name}"
+    deployment_mode = "Incremental"
+    template_body = "${file("../../shared/appservice-scm.template.json")}"
+
+    parameters {
+        name = "${azurerm_template_deployment.mozaik.parameters.name}"
+        repoURL = "https://github.com/noms-digital-studio/overwatch-mozaik.git"
+        branch = "master"
+    }
+
+    depends_on = ["azurerm_template_deployment.mozaik"]
+}
+
+resource "github_repository_webhook" "mozaik-deploy" {
+  repository = "overwatch-mozaik"
+
+  name = "web"
+  configuration {
+    url = "${azurerm_template_deployment.mozaik-github.outputs.deployTrigger}?scmType=GitHub"
+    content_type = "form"
+    insecure_ssl = false
+  }
+  active = true
+
+  events = ["push"]
+}
