d79f49db3df665e408047b277a6123ef0709296a
Glen Mailer <glenjamin@gmail.com>
2017-05-15 12:46:54 +0100

    Configure IIS web logs to storage by script as well

--- iis/prod/iis.tf
+++ iis/prod/iis.tf
@@ -158,6 +158,34 @@ resource "azurerm_template_deployment" "webapp" {
     }
 }
 
+data "external" "sas-url" {
+    program = ["node", "../../tools/container-sas-url.js"]
+    query {
+        subscription_id = "${var.azure_subscription_id}"
+        tenant_id = "${var.azure_tenant_id}"
+        resource_group = "${azurerm_resource_group.group.name}"
+        storage_account = "${azurerm_storage_account.storage.name}"
+        container = "web-logs"
+        permissions = "rwdl"
+        start_date = "2017-05-15T00:00:00Z"
+        end_date = "2217-05-15T00:00:00Z"
+    }
+}
+
+resource "azurerm_template_deployment" "webapp-weblogs" {
+    name = "webapp-weblogs"
+    resource_group_name = "${azurerm_resource_group.group.name}"
+    deployment_mode = "Incremental"
+    template_body = "${file("../../shared/appservice-weblogs.template.json")}"
+
+    parameters {
+        name = "${var.app-name}"
+        storageSAS = "${data.external.sas-url.result.url}"
+    }
+
+    depends_on = ["azurerm_template_deployment.webapp"]
+}
+
 resource "azurerm_template_deployment" "insights" {
     name = "${var.app-name}"
     resource_group_name = "${azurerm_resource_group.group.name}"
