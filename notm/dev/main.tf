755b1d0ad398c78522b23db704b2bbf1bc6b9789
Glen Mailer <glenjamin@gmail.com>
2017-05-18 17:04:26 +0100

    Add dev app for nomis on the move

--- notm/dev/main.tf
+++ notm/dev/main.tf
@@ -10,10 +10,11 @@ terraform {
     }
 }
 
-variable "env-name" {
+variable "app-name" {
     type = "string"
     default = "notm-dev"
 }
+
 variable "tags" {
     type = "map"
     default {
@@ -22,8 +23,142 @@ variable "tags" {
     }
 }
 
+resource "random_id" "session-secret" {
+    byte_length = 40
+}
+
 resource "azurerm_resource_group" "group" {
-    name = "${var.env-name}"
+    name = "${var.app-name}"
     location = "ukwest"
+    tags = "${var.tags}"
+}
+
+resource "azurerm_storage_account" "storage" {
+    name = "${replace(var.app-name, "-", "")}storage"
+    resource_group_name = "${azurerm_resource_group.group.name}"
+    location = "${azurerm_resource_group.group.location}"
+    account_type = "Standard_RAGRS"
+    enable_blob_encryption = true
+
+    tags = "${var.tags}"
+}
+
+variable "log-containers" {
+    type = "list"
+    default = ["web-logs"]
+}
+resource "azurerm_storage_container" "logs" {
+    count = "${length(var.log-containers)}"
+    name = "${var.log-containers[count.index]}"
+    resource_group_name = "${azurerm_resource_group.group.name}"
+    storage_account_name = "${azurerm_storage_account.storage.name}"
+    container_access_type = "private"
+}
+
+resource "azurerm_key_vault" "vault" {
+    name = "${var.app-name}"
+    resource_group_name = "${azurerm_resource_group.group.name}"
+    location = "${azurerm_resource_group.group.location}"
+    sku {
+        name = "standard"
+    }
+    tenant_id = "${var.azure_tenant_id}"
+
+    access_policy {
+        tenant_id = "${var.azure_tenant_id}"
+        object_id = "${var.azure_webops_group_oid}"
+        key_permissions = ["all"]
+        secret_permissions = ["all"]
+    }
+    access_policy {
+        tenant_id = "${var.azure_tenant_id}"
+        object_id = "${var.azure_app_service_oid}"
+        key_permissions = []
+        secret_permissions = ["get"]
+    }
+    access_policy {
+        object_id = "${var.azure_glenm_tf_oid}"
+        tenant_id = "${var.azure_tenant_id}"
+        key_permissions = []
+        secret_permissions = ["get", "list"]
+    }
+    access_policy {
+        object_id = "${var.azure_robl_tf_oid}"
+        tenant_id = "${var.azure_tenant_id}"
+        key_permissions = []
+        secret_permissions = ["get", "list"]
+    }
+
+    enabled_for_deployment = false
+    enabled_for_disk_encryption = false
+    enabled_for_template_deployment = true
+
+    tags = "${var.tags}"
+}
+
+resource "azurerm_template_deployment" "webapp" {
+    name = "webapp"
+    resource_group_name = "${azurerm_resource_group.group.name}"
+    deployment_mode = "Incremental"
+    template_body = "${file("../../shared/appservice.template.json")}"
+    parameters {
+        name = "${var.app-name}"
+        service = "${var.tags["Service"]}"
+        environment = "${var.tags["Environment"]}"
+    }
+}
+
+data "external" "sas-url" {
+    program = ["node", "../../tools/container-sas-url.js"]
+    query {
+        subscription_id = "${var.azure_subscription_id}"
+        tenant_id = "${var.azure_tenant_id}"
+        resource_group = "${azurerm_resource_group.group.name}"
+        storage_account = "${azurerm_storage_account.storage.name}"
+        container = "web-logs"
+        permissions = "rwdl"
+        start_date = "2017-05-15T00:00:00Z"
+        end_date = "2217-05-15T00:00:00Z"
+    }
+}
+
+resource "azurerm_template_deployment" "webapp-weblogs" {
+    name = "webapp-weblogs"
+    resource_group_name = "${azurerm_resource_group.group.name}"
+    deployment_mode = "Incremental"
+    template_body = "${file("../../shared/appservice-weblogs.template.json")}"
+
+    parameters {
+        name = "${var.app-name}"
+        storageSAS = "${data.external.sas-url.result["url"]}"
+    }
+
+    depends_on = ["azurerm_template_deployment.webapp"]
+}
+
+resource "azurerm_template_deployment" "webapp-config" {
+    name = "webapp-config"
+    resource_group_name = "${azurerm_resource_group.group.name}"
+    deployment_mode = "Incremental"
+    template_body = "${file("../webapp-config.template.json")}"
+
+    parameters {
+        name = "${var.app-name}"
+        NODE_ENV = "production"
+        NOMIS_URL = "TODO: this"
+        NOMIS_TOKEN = "TODO: this"
+        NOMIS_PRIVATE_KEY = "TODO: this"
+        SESSION_SECRET = "${random_id.session-secret.b64}"
+    }
+
+    depends_on = ["azurerm_template_deployment.webapp"]
+}
+
+resource "azurerm_dns_cname_record" "cname" {
+    name = "notm-dev"
+    zone_name = "hmpps.dsd.io"
+    resource_group_name = "webops"
+    ttl = "300"
+    record = "${var.app-name}.azurewebsites.net"
     tags = "${var.tags}"
 }
