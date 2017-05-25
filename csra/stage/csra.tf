4be5f556444a3d548350519753a1a0958fee449f
Glen Mailer <glenjamin@gmail.com>
2017-05-25 16:11:02 +0100

    Create application insights resource for IIS stage
    https://trello.com/c/by0E8COh/137-help-csra-get-their-logging-set-up

--- csra/stage/csra.tf
+++ csra/stage/csra.tf
@@ -28,6 +28,28 @@ resource "azurerm_resource_group" "group" {
     tags = "${var.tags}"
 }
 
+resource "azurerm_storage_account" "storage" {
+    name = "${replace(var.app-name, "-", "")}storage"
+    resource_group_name = "${azurerm_resource_group.group.name}"
+    location = "${azurerm_resource_group.group.location}"
+    account_type = "Standard_RAGRS"
+    enable_blob_encryption = true
+
+    tags = "${var.tags}"
+}
+
+variable "log-containers" {
+    type = "list"
+    default = ["web-logs"]
+}
+resource "azurerm_storage_container" "logs" {
+    count = "${length(var.log-containers)}"
+    name = "${var.log-containers[count.index]}"
+    resource_group_name = "${azurerm_resource_group.group.name}"
+    storage_account_name = "${azurerm_storage_account.storage.name}"
+    container_access_type = "private"
+}
+
 resource "azurerm_template_deployment" "webapp" {
     name = "webapp"
     resource_group_name = "${azurerm_resource_group.group.name}"
@@ -38,6 +60,63 @@ resource "azurerm_template_deployment" "webapp" {
         service = "${var.tags["Service"]}"
         environment = "${var.tags["Environment"]}"
     }
+}
+
+data "external" "sas-url" {
+    program = ["node", "../../tools/container-sas-url.js"]
+    query {
+        subscription_id = "${var.azure_subscription_id}"
+        tenant_id = "${var.azure_tenant_id}"
+        resource_group = "${azurerm_resource_group.group.name}"
+        storage_account = "${azurerm_storage_account.storage.name}"
+        container = "web-logs"
+        permissions = "rwdl"
+        start_date = "2017-05-15T00:00:00Z"
+        end_date = "2217-05-15T00:00:00Z"
+    }
+}
+
+resource "azurerm_template_deployment" "webapp-weblogs" {
+    name = "webapp-weblogs"
+    resource_group_name = "${azurerm_resource_group.group.name}"
+    deployment_mode = "Incremental"
+    template_body = "${file("../../shared/appservice-weblogs.template.json")}"
+
+    parameters {
+        name = "${var.app-name}"
+        storageSAS = "${data.external.sas-url.result["url"]}"
+    }
+
+    depends_on = ["azurerm_template_deployment.webapp"]
+}
+
+resource "azurerm_template_deployment" "insights" {
+    name = "${var.app-name}"
+    resource_group_name = "${azurerm_resource_group.group.name}"
+    deployment_mode = "Incremental"
+    template_body = "${file("../../shared/insights.template.json")}"
+    parameters {
+        name = "${var.app-name}"
+        location = "northeurope" // Not in UK yet
+        service = "${var.tags["Service"]}"
+        environment = "${var.tags["Environment"]}"
+        appServiceId = "${azurerm_template_deployment.webapp.outputs["resourceId"]}"
+    }
+}
+
+resource "azurerm_template_deployment" "webapp-config" {
+    name = "webapp-config"
+    resource_group_name = "${azurerm_resource_group.group.name}"
+    deployment_mode = "Incremental"
+    template_body = "${file("../webapp-config.template.json")}"
+
+    parameters {
+        name = "${var.app-name}"
+        NODE_ENV = "production"
+        APPINSIGHTS_INSTRUMENTATIONKEY = "${azurerm_template_deployment.insights.outputs["instrumentationKey"]}"
+    }
+
+    depends_on = ["azurerm_template_deployment.webapp"]
 }
 
 resource "azurerm_template_deployment" "webapp-hostname" {
