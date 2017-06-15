fcbccd3648519b47e8a1ba38d086629a64c7bd4c
Glen Mailer <glenjamin@gmail.com>
2017-06-15 16:45:01 +0100

    Put the beanstalk application inside a VPC

--- health-kick/stage/main.tf
+++ health-kick/stage/main.tf
@@ -12,11 +12,19 @@ variable "app-name" {
     type = "string"
     default = "health-kick-stage"
 }
+variable "tags" {
+    type = "map"
+    default {
+        Service = "health-kick"
+        Environment = "Stage"
+    }
+}
 
 resource "aws_elastic_beanstalk_application" "app" {
     name = "${var.app-name}"
     description = "${var.app-name}"
 }
+
 
 resource "aws_elastic_beanstalk_environment" "app-env" {
     name = "${var.app-name}"
@@ -35,49 +43,60 @@ resource "aws_elastic_beanstalk_environment" "app-env" {
         value = "aws-elasticbeanstalk-ec2-role"
     }
     setting {
+        namespace = "aws:elasticbeanstalk:application"
+        name = "Application Healthcheck URL"
+        value = "/health"
+    }
+    setting {
         namespace = "aws:elasticbeanstalk:environment"
         name = "ServiceRole"
         value = "aws-elasticbeanstalk-service-role"
     }
-}
+    setting {
+        namespace = "aws:ec2:vpc"
+        name = "VPCId"
+        value = "${aws_vpc.vpc.id}"
+    }
+    setting {
+        namespace = "aws:ec2:vpc"
+        name = "Subnets"
+        value = "${aws_subnet.private-a.id}"
+    }
+    setting {
+        namespace = "aws:ec2:vpc"
+        name = "ELBSubnets"
+        value = "${aws_subnet.public-a.id}"
+    }
+    setting {
+        namespace = "aws:ec2:vpc"
+        name = "AssociatePublicIpAddress"
+        value = "false"
+    }
+    setting {
+        namespace = "aws:elasticbeanstalk:healthreporting:system"
+        name = "SystemType"
+        value = "enhanced"
+    }
+    setting {
+        namespace = "aws:elasticbeanstalk:managedactions"
+        name = "ManagedActionsEnabled"
+        value = "true"
+    }
+    setting {
+        namespace = "aws:elasticbeanstalk:managedactions"
+        name = "PreferredStartTime"
+        value = "Fri:10:00"
+    }
+    setting {
+        namespace = "aws:elasticbeanstalk:managedactions:platformupdate"
+        name = "UpdateLevel"
+        value = "minor"
+    }
+    setting {
+        namespace = "aws:elasticbeanstalk:managedactions:platformupdate"
+        name = "InstanceRefreshEnabled"
+        value = "true"
+    }
 
-
-resource "aws_iam_user" "deployer" {
-    name = "${var.app-name}-deployer"
-}
-resource "aws_iam_group_membership" "ci" {
-    name = "${aws_iam_user.deployer.name}"
-    users = ["${aws_iam_user.deployer.name}"]
-    group = "${aws_iam_group.deployers.name}"
-}
-
-resource "aws_iam_user_policy" "deployer" {
-    name = "${var.app-name}-deployer"
-    user = "${aws_iam_user.deployer.name}"
-    # Based on https://gist.github.com/magnetikonline/5034bdbb049181a96ac9
-    # and https://gist.github.com/jakubholynet/0055cf69b5b2a9554af67a11828209a5
-    policy = <<JSON
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Action": [
-                "elasticbeanstalk:*"
-            ],
-            "Effect": "Allow",
-            "Resource": [
-                "arn:aws:elasticbeanstalk:*::solutionstack/*",
-                "arn:aws:elasticbeanstalk:${var.aws_region}:${var.aws_account_id}:application/${aws_elastic_beanstalk_application.app.name}",
-                "arn:aws:elasticbeanstalk:${var.aws_region}:${var.aws_account_id}:applicationversion/${aws_elastic_beanstalk_application.app.name}/*",
-                "arn:aws:elasticbeanstalk:${var.aws_region}:${var.aws_account_id}:environment/${aws_elastic_beanstalk_application.app.name}/*",
-                "arn:aws:elasticbeanstalk:${var.aws_region}:${var.aws_account_id}:template/${aws_elastic_beanstalk_application.app.name}/*"
-            ]
-        }
-    ]
-}
-JSON
-}
-
-resource "aws_iam_access_key" "deployer" {
-    user = "${aws_iam_user.deployer.name}"
+    tags = "${var.tags}"
 }
