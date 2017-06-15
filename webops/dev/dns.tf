d7e152a24d012cfb6266b440b7480edcf074c201
Glen Mailer <glenjamin@gmail.com>
2017-06-15 16:51:11 +0100

    Friendly hostname for health-kick in aws

--- webops/dev/dns.tf
+++ webops/dev/dns.tf
@@ -71,3 +71,11 @@ resource "azurerm_dns_cname_record" "search" {
     ttl = "300"
     record = "search-noms-api.dsd.io"
 }
+
+resource "azurerm_dns_cname_record" "health-kick" {
+    name = "health-kick"
+    zone_name = "${azurerm_dns_zone.hmpps.name}"
+    resource_group_name = "${azurerm_resource_group.webops.name}"
+    ttl = "60"
+    record = "health-kick-stage.m3mmmva2gt.eu-west-2.elasticbeanstalk.com"
+}
