cf1f69839fa7d870d1161eed14f96cc23855ad59
Glen Mailer <glenjamin@gmail.com>
2017-06-28 15:48:27 +0100

    delegate some DNS to WMT

--- webops/dev/dns.tf
+++ webops/dev/dns.tf
@@ -64,6 +64,30 @@ resource "azurerm_dns_ns_record" "digital-prisons" {
     }
 }
 
+resource "azurerm_dns_ns_record" "wmt" {
+    name = "wmt"
+    zone_name = "${azurerm_dns_zone.hmpps.name}"
+    resource_group_name = "${azurerm_resource_group.webops.name}"
+    ttl = "300"
+
+    record {
+        nsdname = "ns1-01.azure-dns.com."
+    }
+    record {
+        nsdname = "ns2-01.azure-dns.net."
+    }
+    record {
+        nsdname = "ns3-01.azure-dns.org."
+    }
+    record {
+        nsdname = "ns4-01.azure-dns.info."
+    }
+    tags {
+        Service = "WebOps"
+        Environment = "Management"
+    }
+}
+
 resource "azurerm_dns_cname_record" "search" {
     name = "search"
     zone_name = "${azurerm_dns_zone.noms.name}"
