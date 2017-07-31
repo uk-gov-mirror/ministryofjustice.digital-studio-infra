ed0cd9430845c4b9fb271debb4e92a43dc42fdc6
Glen Mailer <glenjamin@gmail.com>
2017-07-31 11:46:09 +0100

    Deploy slackhook app into dev subscription

--- webops/dev/main.tf
+++ webops/dev/main.tf
@@ -10,11 +10,64 @@ terraform {
     }
 }
 
-resource "azurerm_resource_group" "webops" {
-  name = "webops"
-  location = "ukwest"
-  tags {
-    Service = "WebOps"
-    Environment = "Management"
-  }
+variable "tags" {
+    type = "map"
+    default {
+        Service = "WebOps"
+        Environment = "Management"
+    }
+}
+
+resource "azurerm_resource_group" "group" {
+    name = "webops"
+    location = "ukwest"
+    tags = "${var.tags}"
+}
+
+resource "azurerm_key_vault" "vault" {
+    name = "webops-dev"
+    resource_group_name = "${azurerm_resource_group.group.name}"
+    location = "${azurerm_resource_group.group.location}"
+    sku {
+        name = "standard"
+    }
+    tenant_id = "${var.azure_tenant_id}"
+
+    access_policy {
+        tenant_id = "${var.azure_tenant_id}"
+        object_id = "${var.azure_webops_group_oid}"
+        key_permissions = ["all"]
+        secret_permissions = ["all"]
+    }
+    access_policy {
+        tenant_id = "${var.azure_tenant_id}"
+        object_id = "${var.azure_app_service_oid}"
+        key_permissions = []
+        secret_permissions = ["get"]
+    }
+    access_policy {
+        object_id = "${var.azure_glenm_tf_oid}"
+        tenant_id = "${var.azure_tenant_id}"
+        key_permissions = []
+        secret_permissions = ["get", "set"]
+    }
+    access_policy {
+        object_id = "${var.azure_robl_tf_oid}"
+        tenant_id = "${var.azure_tenant_id}"
+        key_permissions = []
+        secret_permissions = ["get", "set"]
+    }
+    access_policy {
+        object_id = "${var.slackhook_app_oid}"
+        tenant_id = "${var.azure_tenant_id}"
+        key_permissions = []
+        secret_permissions = ["get"]
+    }
+
+    enabled_for_deployment = false
+    enabled_for_disk_encryption = false
+    enabled_for_template_deployment = true
+
+    tags = "${var.tags}"
+
 }
