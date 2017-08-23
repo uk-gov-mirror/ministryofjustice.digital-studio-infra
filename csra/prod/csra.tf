7726f8814f04b2be71d5ea806aaa823f47fef9bc
Rob Lazzurs <rob@lazzurs.org>
2017-08-23 10:57:37 +0100

    Upping the number of workers on CSRA.
    Moving to two workers to allow the platform to make changes without
    causing us downtime.

--- csra/prod/csra.tf
+++ csra/prod/csra.tf
@@ -144,7 +144,7 @@ resource "azurerm_template_deployment" "webapp" {
         name = "${var.app-name}"
         service = "${var.tags["Service"]}"
         environment = "${var.tags["Environment"]}"
-        workers = "1"
+        workers = "2"
     }
 }
 
