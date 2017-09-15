76138f4074bf0bd82f1a538762c530de3b3f4cc0
Glen Mailer <glenjamin@gmail.com>
2017-09-15 15:35:40 +0100

    Delegate some production dns to the nomis-api team

--- webops/prod/dns.tf
+++ webops/prod/dns.tf
@@ -7,6 +7,30 @@ resource "azurerm_dns_zone" "service-hmpps" {
     }
 }
 
+resource "azurerm_dns_ns_record" "nomis-api" {
+    name = "nomis-api"
+    zone_name = "${azurerm_dns_zone.service-hmpps.name}"
+    resource_group_name = "${azurerm_resource_group.group.name}"
+    ttl = "300"
+
+    record {
+        nsdname = "ns1-07.azure-dns.com."
+    }
+    record {
+        nsdname = "ns2-07.azure-dns.net."
+    }
+    record {
+        nsdname = "ns3-07.azure-dns.org."
+    }
+    record {
+        nsdname = "ns4-07.azure-dns.info."
+    }
+    tags {
+        Service = "WebOps"
+        Environment = "Management"
+    }
+}
+
 output "service.hmpps.dsd.io nameservers" {
     value = ["${azurerm_dns_zone.service-hmpps.name_servers}"]
 }
