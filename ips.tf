6b63e4c8821913a3a0bd0934a10dd7e3adfd6a8d
Glen Mailer <glenjamin@gmail.com>
2017-09-20 15:32:23 +0100

    Allow access to NOTM preprod and prod from digital prisons networks

--- ips.tf
+++ ips.tf
@@ -4,5 +4,7 @@ variable "ips" {
         office = "217.33.148.210"
         quantum = "62.25.109.197"
         health-kick = "35.177.252.195"
+        digitalprisons1 = "52.56.112.98"
+        digitalprisons2 = "52.56.118.154"
     }
 }
