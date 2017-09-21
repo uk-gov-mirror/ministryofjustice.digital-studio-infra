0db1eb8774211c08fc8e7e0135eb0f1b8944b6b3
Glen Mailer <glenjamin@gmail.com>
2017-09-21 14:43:21 +0100

    Create mock environment for licences

---
+++ licences/mock/main.tf
@@ -0,0 +1,227 @@
+terraform {
+    required_version = ">= 0.10.5"
+    backend "azurerm" {
+        resource_group_name = "webops"
+        storage_account_name = "nomsstudiowebops"
+        container_name = "terraform"
+        key = "licences-mock.terraform.tfstate"
+        arm_subscription_id = "c27cfedb-f5e9-45e6-9642-0fad1a5c94e7"
+        arm_tenant_id = "747381f4-e81f-4a43-bf68-ced6a1e14edf"
+    }
+}
+
+variable "app-name" {
+    type = "string"
+    default = "licences-mock"
+}
+
+variable "tags" {
+    type = "map"
+    default {
+        Service = "Licences"
+        Environment = "Mock"
+    }
+}
+
+resource "random_id" "session-secret" {
+    byte_length = 40
+}
+
+resource "azurerm_resource_group" "group" {
+    name = "${var.app-name}"
+    location = "ukwest"
+    tags = "${var.tags}"
+}
+
+resource "azurerm_storage_account" "storage" {
+    name = "${replace(var.app-name, "-", "")}storage"
+    resource_group_name = "${azurerm_resource_group.group.name}"
+    location = "${azurerm_resource_group.group.location}"
+    account_type = "Standard_RAGRS"
+    enable_blob_encryption = true
+
+    tags = "${var.tags}"
+}
+
+resource "azurerm_storage_container" "logs" {
+    name = "web-logs"
+    resource_group_name = "${azurerm_resource_group.group.name}"
+    storage_account_name = "${azurerm_storage_account.storage.name}"
+    container_access_type = "private"
+}
+
+resource "azurerm_key_vault" "vault" {
+    name = "${var.app-name}"
+    resource_group_name = "${azurerm_resource_group.group.name}"
+    location = "${azurerm_resource_group.group.location}"
+    sku {
+        name = "standard"
+    }
+    tenant_id = "${var.azure_tenant_id}"
+
+    access_policy {
+        tenant_id = "${var.azure_tenant_id}"
+        object_id = "${var.azure_webops_group_oid}"
+        key_permissions = ["all"]
+        secret_permissions = ["all"]
+    }
+    access_policy {
+        tenant_id = "${var.azure_tenant_id}"
+        object_id = "${var.azure_app_service_oid}"
+        key_permissions = []
+        secret_permissions = ["get"]
+    }
+    access_policy {
+        object_id = "${var.azure_glenm_tf_oid}"
+        tenant_id = "${var.azure_tenant_id}"
+        key_permissions = []
+        secret_permissions = ["get", "set"]
+    }
+    access_policy {
+        object_id = "${var.azure_robl_tf_oid}"
+        tenant_id = "${var.azure_tenant_id}"
+        key_permissions = []
+        secret_permissions = ["get", "set"]
+    }
+
+    enabled_for_deployment = false
+    enabled_for_disk_encryption = false
+    enabled_for_template_deployment = true
+
+    tags = "${var.tags}"
+}
+
+resource "azurerm_template_deployment" "webapp" {
+    name = "webapp"
+    resource_group_name = "${azurerm_resource_group.group.name}"
+    deployment_mode = "Incremental"
+    template_body = "${file("../../shared/appservice.template.json")}"
+    parameters {
+        name = "${var.app-name}"
+        service = "${var.tags["Service"]}"
+        environment = "${var.tags["Environment"]}"
+        workers = "1"
+        sku_name = "S1"
+        sku_tier = "Standard"
+    }
+}
+
+data "external" "sas-url" {
+    program = ["node", "../../tools/container-sas-url.js"]
+    query {
+        subscription_id = "${var.azure_subscription_id}"
+        tenant_id = "${var.azure_tenant_id}"
+        resource_group = "${azurerm_resource_group.group.name}"
+        storage_account = "${azurerm_storage_account.storage.name}"
+        container = "web-logs"
+        permissions = "rwdl"
+        start_date = "2017-05-15T00:00:00Z"
+        end_date = "2217-05-15T00:00:00Z"
+    }
+}
+
+resource "azurerm_template_deployment" "webapp-weblogs" {
+    name = "webapp-weblogs"
+    resource_group_name = "${azurerm_resource_group.group.name}"
+    deployment_mode = "Incremental"
+    template_body = "${file("../../shared/appservice-weblogs.template.json")}"
+
+    parameters {
+        name = "${var.app-name}"
+        storageSAS = "${data.external.sas-url.result["url"]}"
+    }
+
+    depends_on = ["azurerm_template_deployment.webapp"]
+}
+
+resource "azurerm_template_deployment" "insights" {
+    name = "${var.app-name}"
+    resource_group_name = "${azurerm_resource_group.group.name}"
+    deployment_mode = "Incremental"
+    template_body = "${file("../../shared/insights.template.json")}"
+    parameters {
+        name = "${var.app-name}"
+        location = "northeurope" // Not in UK yet
+        service = "${var.tags["Service"]}"
+        environment = "${var.tags["Environment"]}"
+        appServiceId = "${azurerm_template_deployment.webapp.outputs["resourceId"]}"
+    }
+}
+
+resource "azurerm_template_deployment" "webapp-config" {
+    name = "webapp-config"
+    resource_group_name = "${azurerm_resource_group.group.name}"
+    deployment_mode = "Incremental"
+    template_body = "${file("../webapp-config.template.json")}"
+
+    parameters {
+        name = "${var.app-name}"
+        NODE_ENV = "production"
+        SESSION_SECRET = "${random_id.session-secret.b64}"
+        APPINSIGHTS_INSTRUMENTATIONKEY = "${azurerm_template_deployment.insights.outputs["instrumentationKey"]}"
+    }
+
+    depends_on = ["azurerm_template_deployment.webapp"]
+}
+
+resource "azurerm_template_deployment" "webapp-ssl" {
+    name = "webapp-ssl"
+    resource_group_name = "${azurerm_resource_group.group.name}"
+    deployment_mode = "Incremental"
+    template_body = "${file("../../shared/appservice-ssl.template.json")}"
+
+    parameters {
+        name = "${azurerm_template_deployment.webapp.parameters.name}"
+        hostname = "${azurerm_dns_cname_record.cname.name}.${azurerm_dns_cname_record.cname.zone_name}"
+        keyVaultId = "${azurerm_key_vault.vault.id}"
+        keyVaultCertName = "${replace("${azurerm_dns_cname_record.cname.name}.${azurerm_dns_cname_record.cname.zone_name}", ".", "DOT")}"
+        service = "${var.tags["Service"]}"
+        environment = "${var.tags["Environment"]}"
+    }
+
+    depends_on = ["azurerm_template_deployment.webapp"]
+}
+
+resource "azurerm_template_deployment" "webapp-github" {
+    name = "webapp-github"
+    resource_group_name = "${azurerm_resource_group.group.name}"
+    deployment_mode = "Incremental"
+    template_body = "${file("../../shared/appservice-scm.template.json")}"
+
+    parameters {
+        name = "${var.app-name}"
+        repoURL = "https://github.com/noms-digital-studio/licences"
+        branch = "deploy-to-mock"
+    }
+
+    depends_on = ["azurerm_template_deployment.webapp"]
+}
+
+resource "github_repository_webhook" "webapp-deploy" {
+  repository = "licences"
+
+  name = "web"
+  configuration {
+    url = "${azurerm_template_deployment.webapp-github.outputs["deployTrigger"]}?scmType=GitHub"
+    content_type = "form"
+    insecure_ssl = false
+  }
+  active = true
+
+  events = ["push"]
+}
+
+module "slackhook" {
+    source = "../../shared/modules/slackhook"
+    app_name = "${azurerm_template_deployment.webapp.parameters.name}"
+    channels = ["licences"]
+}
+
+resource "azurerm_dns_cname_record" "cname" {
+    name = "licences-mock"
+    zone_name = "hmpps.dsd.io"
+    resource_group_name = "webops"
+    ttl = "300"
+    record = "${var.app-name}.azurewebsites.net"
+    tags = "${var.tags}"
+}
