1c5919abb20b6cf78dbf665bf2de30f97b73f80d
Alistair Todd <alistair.todd@digital.justice.gov.uk>
2017-10-25 11:09:49 +0100

    Licences stage environment

---
+++ licences/stage/main.tf
@@ -0,0 +1,159 @@
+terraform {
+    required_version = ">= 0.10.5"
+    backend "azurerm" {
+        resource_group_name = "webops"
+        storage_account_name = "nomsstudiowebops"
+        container_name = "terraform"
+        key = "licences-stage.terraform.tfstate"
+        arm_subscription_id = "c27cfedb-f5e9-45e6-9642-0fad1a5c94e7"
+        arm_tenant_id = "747381f4-e81f-4a43-bf68-ced6a1e14edf"
+    }
+}
+
+variable "env-name" {
+    type = "string"
+    default = "licences-stage"
+}
+
+variable "tags" {
+    type = "map"
+    default {
+        Service = "Licences"
+        Environment = "Stage"
+    }
+}
+
+resource "random_id" "session-secret" {
+    byte_length = 40
+}
+
+resource "random_id" "sql-ui-password" {
+    byte_length = 32
+}
+
+resource "random_id" "sql-api-password" {
+    byte_length = 32
+}
+
+resource "azurerm_resource_group" "group" {
+    name = "${var.env-name}"
+    location = "ukwest"
+    tags = "${var.tags}"
+}
+
+resource "azurerm_storage_account" "storage" {
+    name = "${replace(var.env-name, "-", "")}storage"
+    resource_group_name = "${azurerm_resource_group.group.name}"
+    location = "${azurerm_resource_group.group.location}"
+    account_type = "Standard_RAGRS"
+    enable_blob_encryption = true
+
+    tags = "${var.tags}"
+}
+
+resource "azurerm_storage_container" "logs" {
+    name = "web-logs"
+    resource_group_name = "${azurerm_resource_group.group.name}"
+    storage_account_name = "${azurerm_storage_account.storage.name}"
+    container_access_type = "private"
+}
+
+resource "azurerm_key_vault" "vault" {
+    name = "${var.env-name}"
+    resource_group_name = "${azurerm_resource_group.group.name}"
+    location = "${azurerm_resource_group.group.location}"
+    sku {
+        name = "standard"
+    }
+    tenant_id = "${var.azure_tenant_id}"
+
+    access_policy {
+        tenant_id = "${var.azure_tenant_id}"
+        object_id = "${var.azure_webops_group_oid}"
+        key_permissions = ["all"]
+        secret_permissions = ["all"]
+    }
+    access_policy {
+        tenant_id = "${var.azure_tenant_id}"
+        object_id = "${var.azure_app_service_oid}"
+        key_permissions = []
+        secret_permissions = ["get"]
+    }
+    access_policy {
+        object_id = "${var.azure_glenm_tf_oid}"
+        tenant_id = "${var.azure_tenant_id}"
+        key_permissions = []
+        secret_permissions = ["get", "set"]
+    }
+    access_policy {
+        object_id = "${var.azure_robl_tf_oid}"
+        tenant_id = "${var.azure_tenant_id}"
+        key_permissions = []
+        secret_permissions = ["get", "set"]
+    }
+
+    enabled_for_deployment = false
+    enabled_for_disk_encryption = false
+    enabled_for_template_deployment = true
+
+    tags = "${var.tags}"
+}
+
+module "sql" {
+    source = "../../shared/modules/azure-sql"
+    name = "${var.env-name}"
+    resource_group = "${azurerm_resource_group.group.name}"
+    location = "${azurerm_resource_group.group.location}"
+    administrator_login = "licences"
+    firewall_rules = [
+        {
+            label = "Open to the world"
+            start = "0.0.0.0"
+            end = "255.255.255.255"
+        },
+    ]
+    audit_storage_account = "${azurerm_storage_account.storage.name}"
+    edition = "Basic"
+    collation = "SQL_Latin1_General_CP1_CI_AS"
+    tags = "${var.tags}"
+
+    db_users = {
+        ui = "${random_id.sql-ui-password.b64}",
+        api = "${random_id.sql-api-password.b64}"
+    }
+
+    setup_queries = [
+        "DROP USER app",
+        "ALTER ROLE db_datareader ADD MEMBER ui",
+        "ALTER ROLE db_datawriter ADD MEMBER ui",
+        "ALTER ROLE db_datareader ADD MEMBER api",
+        "ALTER ROLE db_datawriter ADD MEMBER api"
+    ]
+}
+
+data "external" "sas-url" {
+    program = ["node", "../../tools/container-sas-url.js"]
+    query {
+        subscription_id = "${var.azure_subscription_id}"
+        tenant_id = "${var.azure_tenant_id}"
+        resource_group = "${azurerm_resource_group.group.name}"
+        storage_account = "${azurerm_storage_account.storage.name}"
+        container = "web-logs"
+        permissions = "rwdl"
+        start_date = "2017-05-15T00:00:00Z"
+        end_date = "2217-05-15T00:00:00Z"
+    }
+}
+
+resource "azurerm_template_deployment" "insights" {
+    name = "${var.env-name}"
+    resource_group_name = "${azurerm_resource_group.group.name}"
+    deployment_mode = "Incremental"
+    template_body = "${file("../../shared/insights.template.json")}"
+    parameters {
+        name = "${var.env-name}"
+        location = "northeurope" // Not in UK yet
+        service = "${var.tags["Service"]}"
+        environment = "${var.tags["Environment"]}"
+    }
+}
---
+++ licences/stage/ui.tf
@@ -0,0 +1,126 @@
+variable "ui-name" {
+  type = "string"
+  default = "licences-stage"
+}
+
+resource "azurerm_template_deployment" "ui" {
+  name = "ui"
+  resource_group_name = "${azurerm_resource_group.group.name}"
+  deployment_mode = "Incremental"
+  template_body = "${file("../../shared/appservice.template.json")}"
+  parameters {
+    name = "${var.ui-name}"
+    service = "${var.tags["Service"]}"
+    environment = "${var.tags["Environment"]}"
+    workers = "1"
+    sku_name = "S2"
+    sku_tier = "Standard"
+  }
+}
+
+data "external" "vault" {
+  program = ["node", "../../tools/keyvault-data.js"]
+  query {
+    vault = "${azurerm_key_vault.vault.name}"
+
+    elite_api_gateway_token = "elite-api-gateway-token"
+    elite_api_gateway_private_key = "elite-api-gateway-private-key"
+  }
+}
+
+resource "azurerm_template_deployment" "ui-config" {
+  name = "ui-config"
+  resource_group_name = "${azurerm_resource_group.group.name}"
+  deployment_mode = "Incremental"
+  template_body = "${file("../ui-config.template.json")}"
+
+  parameters {
+    name = "${var.ui-name}"
+    NODE_ENV = "production"
+    SESSION_SECRET = "${random_id.session-secret.b64}"
+    APPINSIGHTS_INSTRUMENTATIONKEY = "${azurerm_template_deployment.insights.outputs["instrumentationKey"]}"
+    DB_USER = "ui"
+    DB_PASS = "${random_id.sql-ui-password.b64}"
+    DB_SERVER = "${module.sql.db_server}"
+    DB_NAME = "${module.sql.db_name}"
+    NOMIS_API_URL = "https://noms-api-dev.dsd.io/elite2api-stage/",
+    NOMIS_GW_TOKEN = "${data.external.vault.result.elite_api_gateway_token}",
+    NOMIS_GW_KEY = "${data.external.vault.result.elite_api_gateway_private_key}"
+  }
+
+  depends_on = ["azurerm_template_deployment.ui"]
+}
+
+resource "azurerm_template_deployment" "ui-ssl" {
+  name = "ui-ssl"
+  resource_group_name = "${azurerm_resource_group.group.name}"
+  deployment_mode = "Incremental"
+  template_body = "${file("../../shared/appservice-ssl.template.json")}"
+
+  parameters {
+    name = "${azurerm_template_deployment.ui.parameters.name}"
+    hostname = "${azurerm_dns_cname_record.ui.name}.${azurerm_dns_cname_record.ui.zone_name}"
+    keyVaultId = "${azurerm_key_vault.vault.id}"
+    keyVaultCertName = "${replace("${azurerm_dns_cname_record.ui.name}.${azurerm_dns_cname_record.ui.zone_name}", ".", "DOT")}"
+    service = "${var.tags["Service"]}"
+    environment = "${var.tags["Environment"]}"
+  }
+
+  depends_on = ["azurerm_template_deployment.ui"]
+}
+
+resource "azurerm_template_deployment" "ui-github" {
+  name = "ui-github"
+  resource_group_name = "${azurerm_resource_group.group.name}"
+  deployment_mode = "Incremental"
+  template_body = "${file("../../shared/appservice-scm.template.json")}"
+
+  parameters {
+    name = "${var.ui-name}"
+    repoURL = "https://github.com/noms-digital-studio/licences"
+    branch = "deploy-to-stage"
+  }
+
+  depends_on = ["azurerm_template_deployment.ui"]
+}
+
+resource "github_repository_webhook" "ui-deploy" {
+  repository = "licences"
+
+  name = "web"
+  configuration {
+    url = "${azurerm_template_deployment.ui-github.outputs["deployTrigger"]}?scmType=GitHub"
+    content_type = "form"
+    insecure_ssl = false
+  }
+  active = true
+
+  events = ["push"]
+}
+
+resource "azurerm_template_deployment" "ui-weblogs" {
+    name = "ui-weblogs"
+    resource_group_name = "${azurerm_resource_group.group.name}"
+    deployment_mode = "Incremental"
+    template_body = "${file("../../shared/appservice-weblogs.template.json")}"
+
+    parameters {
+        name = "${azurerm_template_deployment.ui.parameters.name}"
+        storageSAS = "${data.external.sas-url.result["url"]}"
+    }
+}
+
+module "slackhook-ui" {
+  source = "../../shared/modules/slackhook"
+  app_name = "${azurerm_template_deployment.ui.parameters.name}"
+  channels = ["licences-dev"]
+}
+
+resource "azurerm_dns_cname_record" "ui" {
+  name = "licences-stage"
+  zone_name = "hmpps.dsd.io"
+  resource_group_name = "webops"
+  ttl = "300"
+  record = "${var.ui-name}.azurewebsites.net"
+  tags = "${var.tags}"
+}
