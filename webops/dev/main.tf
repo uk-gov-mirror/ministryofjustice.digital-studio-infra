66aeb23654fafcc730a7a3d725faa3891a90f355
Matt Whittaker <matt.whittaker@digital.justice.gov.uk>
2017-12-13 17:32:26 +0000

    first go at replacing service principal auth with az CLI auth

--- webops/dev/main.tf
+++ webops/dev/main.tf
@@ -1,73 +1,53 @@
-terraform {
-    required_version = ">= 0.9.0"
-    backend "azure" {
-        resource_group_name = "webops"
-        storage_account_name = "nomsstudiowebops"
-        container_name = "terraform"
-        key = "webops-dev.terraform.tfstate"
-        arm_subscription_id = "c27cfedb-f5e9-45e6-9642-0fad1a5c94e7"
-        arm_tenant_id = "747381f4-e81f-4a43-bf68-ced6a1e14edf"
-    }
-}
-
 variable "tags" {
-    type = "map"
-    default {
-        Service = "WebOps"
-        Environment = "Management"
-    }
+  type = "map"
+
+  default {
+    Service     = "WebOps"
+    Environment = "Management"
+  }
 }
 
 resource "azurerm_resource_group" "group" {
-    name = "webops"
-    location = "ukwest"
-    tags = "${var.tags}"
+  name     = "webops"
+  location = "ukwest"
+  tags     = "${var.tags}"
 }
 
 resource "azurerm_key_vault" "vault" {
-    name = "webops-dev"
-    resource_group_name = "${azurerm_resource_group.group.name}"
-    location = "${azurerm_resource_group.group.location}"
-    sku {
-        name = "standard"
-    }
-    tenant_id = "${var.azure_tenant_id}"
+  name                = "webops-dev"
+  resource_group_name = "${azurerm_resource_group.group.name}"
+  location            = "${azurerm_resource_group.group.location}"
 
-    access_policy {
-        tenant_id = "${var.azure_tenant_id}"
-        object_id = "${var.azure_webops_group_oid}"
-        key_permissions = ["all"]
-        secret_permissions = ["all"]
-    }
-    access_policy {
-        tenant_id = "${var.azure_tenant_id}"
-        object_id = "${var.azure_app_service_oid}"
-        key_permissions = []
-        secret_permissions = ["get"]
-    }
-    access_policy {
-        object_id = "${var.azure_glenm_tf_oid}"
-        tenant_id = "${var.azure_tenant_id}"
-        key_permissions = []
-        secret_permissions = ["get", "set"]
-    }
-    access_policy {
-        object_id = "${var.azure_robl_tf_oid}"
-        tenant_id = "${var.azure_tenant_id}"
-        key_permissions = []
-        secret_permissions = ["get", "set"]
-    }
-    access_policy {
-        object_id = "${var.slackhook_app_oid}"
-        tenant_id = "${var.azure_tenant_id}"
-        key_permissions = []
-        secret_permissions = ["get"]
-    }
+  sku {
+    name = "standard"
+  }
 
-    enabled_for_deployment = false
-    enabled_for_disk_encryption = false
-    enabled_for_template_deployment = true
+  tenant_id = "${var.azure_tenant_id}"
 
-    tags = "${var.tags}"
+  access_policy {
+    tenant_id          = "${var.azure_tenant_id}"
+    object_id          = "${var.azure_webops_group_oid}"
+    key_permissions    = []
+    secret_permissions = "${var.azure_secret_permissions_all}"
+  }
 
+  access_policy {
+    tenant_id          = "${var.azure_tenant_id}"
+    object_id          = "${var.azure_app_service_oid}"
+    key_permissions    = []
+    secret_permissions = ["get"]
+  }
+
+  access_policy {
+    object_id          = "${var.slackhook_app_oid}"
+    tenant_id          = "${var.azure_tenant_id}"
+    key_permissions    = []
+    secret_permissions = ["get"]
+  }
+
+  enabled_for_deployment          = false
+  enabled_for_disk_encryption     = false
+  enabled_for_template_deployment = true
+
+  tags = "${var.tags}"
 }
