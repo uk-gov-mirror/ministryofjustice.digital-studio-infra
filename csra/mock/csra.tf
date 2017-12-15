7ab7a9d9a631ac832aeb65bd8628c3471bf4aab1
John Hackett <john.hackett@digital.justice.gov.uk>
2017-12-15 11:04:36 +0000

    CSRA moved to cli auth

--- csra/mock/csra.tf
+++ csra/mock/csra.tf
@@ -1,15 +1,3 @@
-terraform {
-    required_version = ">= 0.9.2"
-    backend "azure" {
-        resource_group_name = "webops"
-        storage_account_name = "nomsstudiowebops"
-        container_name = "terraform"
-        key = "csra-mock.terraform.tfstate"
-        arm_subscription_id = "c27cfedb-f5e9-45e6-9642-0fad1a5c94e7"
-        arm_tenant_id = "747381f4-e81f-4a43-bf68-ced6a1e14edf"
-    }
-}
-
 variable "app-name" {
     type = "string"
     default = "csra-mock"
@@ -39,7 +27,8 @@ resource "azurerm_storage_account" "storage" {
     name = "${replace(var.app-name, "-", "")}storage"
     resource_group_name = "${azurerm_resource_group.group.name}"
     location = "${azurerm_resource_group.group.location}"
-    account_type = "Standard_RAGRS"
+    account_tier = "Standard"
+    account_replication_type = "RAGRS"
     enable_blob_encryption = true
 
     tags = "${var.tags}"
@@ -69,26 +58,14 @@ resource "azurerm_key_vault" "vault" {
     access_policy {
         tenant_id = "${var.azure_tenant_id}"
         object_id = "${var.azure_webops_group_oid}"
-        key_permissions = ["all"]
-        secret_permissions = ["all"]
+        key_permissions = []
+        secret_permissions = ["${var.azure_secret_permissions_all}"]
     }
     access_policy {
         tenant_id = "${var.azure_tenant_id}"
         object_id = "${var.azure_app_service_oid}"
         key_permissions = []
         secret_permissions = ["get"]
-    }
-    access_policy {
-        object_id = "${var.azure_glenm_tf_oid}"
-        tenant_id = "${var.azure_tenant_id}"
-        key_permissions = []
-        secret_permissions = ["get", "set"]
-    }
-    access_policy {
-        object_id = "${var.azure_robl_tf_oid}"
-        tenant_id = "${var.azure_tenant_id}"
-        key_permissions = []
-        secret_permissions = ["get", "set"]
     }
 
     enabled_for_deployment = false
@@ -142,17 +119,18 @@ resource "azurerm_template_deployment" "webapp" {
 }
 
 data "external" "sas-url" {
-    program = ["node", "../../tools/container-sas-url.js"]
-    query {
-        subscription_id = "${var.azure_subscription_id}"
-        tenant_id = "${var.azure_tenant_id}"
-        resource_group = "${azurerm_resource_group.group.name}"
-        storage_account = "${azurerm_storage_account.storage.name}"
-        container = "web-logs"
-        permissions = "rwdl"
-        start_date = "2017-05-15T00:00:00Z"
-        end_date = "2217-05-15T00:00:00Z"
-    }
+  program = ["node", "../../tools/container-sas-url-cli-auth.js"]
+
+  query {
+    subscription_id = "${var.azure_subscription_id}"
+    tenant_id       = "${var.azure_tenant_id}"
+    resource_group  = "${azurerm_resource_group.group.name}"
+    storage_account = "${azurerm_storage_account.storage.name}"
+    container       = "web-logs"
+    permissions     = "rwdl"
+    start_date = "2017-05-15T00:00:00Z"
+    end_date = "2217-05-15T00:00:00Z"
+  }
 }
 
 resource "azurerm_template_deployment" "webapp-weblogs" {
@@ -184,7 +162,7 @@ resource "azurerm_template_deployment" "insights" {
 }
 
 data "external" "vault" {
-    program = ["node", "../../tools/keyvault-data.js"]
+    program = ["node", "../../tools/keyvault-data-cli-auth.js"]
     query {
         vault = "${azurerm_key_vault.vault.name}"
 
