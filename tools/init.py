01c4cecd6c981f05f8c693f1100dac6309955b8a
simoncross747 <simon.cross@digital.justice.gov.uk>
2018-01-18 12:48:23 +0000

    init.py refactored to use modules for common functions

--- tools/init.py
+++ tools/init.py
@@ -6,6 +6,7 @@ import os.path
 import shutil
 
 from python_modules import state_backup
+from python_modules import azure_account
 
 gitRoot = subprocess.run(
     ["git", "rev-parse", "--show-toplevel"],
@@ -34,15 +35,15 @@ if not os.path.isfile("./dso.json"):
     shutil.copy2(src, dst)
 
 appEnvConfig = json.load(open("./dso.json"))
-    
+
 providerConfig = json.load(
     open(gitRoot + "/tools/config/azure-provider-config.json"))
 
 if 'storage_account_name' in appEnvConfig:
-  providerConfig[environment]["storage_account_name"] = appEnvConfig['storage_account_name']
+    providerConfig[environment]["storage_account_name"] = appEnvConfig['storage_account_name']
 
 if 'resource_group_name' in appEnvConfig:
-  providerConfig[environment]["resource_group_name"] = appEnvConfig['resource_group_name']
+    providerConfig[environment]["resource_group_name"] = appEnvConfig['resource_group_name']
 
 configTfJson = {
     'terraform': {
@@ -65,8 +66,8 @@ configTfJson = {
     }
 }
 
-if os.path.exists("./.terraform"): 
-  state_backup.backup()
+if os.path.exists("./.terraform"):
+    state_backup.backup()
 
 jsonFile = json.dumps(configTfJson, indent=2)
 
@@ -75,20 +76,12 @@ with open("config.tf.json", "w") as f:
 
 config = json.load(open("./config.tf.json"))
 
-subscription_params = ["--subscription",
-                       providerConfig[environment]["subscription_id"]]
-
 # Ensure that CLI is logged in and can access the relevant subscription
-subprocess.run(
-    ["az", "account", "get-access-token", *subscription_params],
-    check=True, stdout=subprocess.DEVNULL
-)
+azure_account.azure_access_token(providerConfig[environment]["subscription_id"])
 
 # Activate relevant subscription in CLI
-subprocess.run(
-    ["az", "account", "set", *subscription_params],
-    check=True
-)
+azure_account.azure_set_subscription(
+    providerConfig[environment]["subscription_id"])
 
 # Extract storage account key for remote state
 key = subprocess.run(
@@ -106,22 +99,22 @@ key = subprocess.run(
 
 response = json.loads(subprocess.run(
     ["az", "storage", "container", "exists",
-    "--account-name", providerConfig[environment]["storage_account_name"],
-    "--name", "terraform",
-    ],
+     "--account-name", providerConfig[environment]["storage_account_name"],
+     "--name", "terraform",
+     ],
     stdout=subprocess.PIPE,
     check=True
 ).stdout.decode())
 
 if response["exists"] == False:
-  subprocess.run(
-    ["az", "storage","container", "create",
-    "--account-name", providerConfig[environment]["storage_account_name"],
-    "--name", "terraform"
-    ],
-    stdout=subprocess.PIPE,
-    check=True
-  )
+    subprocess.run(
+        ["az", "storage", "container", "create",
+         "--account-name", providerConfig[environment]["storage_account_name"],
+         "--name", "terraform"
+         ],
+        stdout=subprocess.PIPE,
+        check=True
+    )
 
 # Init terraform with acquired storage account key
 subprocess.run(
