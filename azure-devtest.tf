d95c308e58e0cd2607f67a0ca090dea612eb8406
John Hackett <john.hackett@digital.justice.gov.uk>
2018-01-22 14:44:03 +0000

    Adding OIDs for different teams

--- azure-devtest.tf
+++ azure-devtest.tf
@@ -14,6 +14,31 @@ variable "azure_webops_group_oid" {
     type = "string"
     default = "98dc3307-f515-4717-b3c1-7174413e20b0"
 }
+variable "azure_notm_group_oid" {
+	type = "string"
+	default = "1687acb2-20d2-4b0c-96c0-9be788cdcab9"
+}
+variable "azure_csra_group_oid" {
+	type = "string"
+	default = "531fa282-da48-4ba5-9373-e1d1a1836f00"
+}
+variable "azure_digital_hub_group_oid" {
+	type = "string"
+	default = "5914163b-54b9-4ad1-b04c-debf07720233"
+}
+variable "azure_nomis_api_group_oid" {
+	type = "string"
+	default = "c9028ae9-59e2-46c5-8ea6-2eba74271d86"
+}
+variable "azure_iis_group_oid" {
+	type = "string"
+	default = "cce161c1-f0fb-499f-bbef-cf7788cc3928"
+}
+variable "azure_nomis_aap_group_oid" {
+	type = "string"
+	default = "e48a63e8-9b32-427a-8cd5-12b5faacb50a"
+}
+
 
 // These AD ObjectIDs were found via `az ad sp list`
 variable "azure_glenm_tf_oid" {
