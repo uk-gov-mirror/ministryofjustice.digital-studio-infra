fde9bb4d2fe5ee7cdef146ab2142034f673cf5e7
Simon Cross <simon.cross@digital.justice.gov.uk>
2018-02-16 16:54:30 +0000

    Added bootstrapping a new app to README and a scripted version

---
+++ tools/config/main.tf
@@ -0,0 +1,29 @@
+variable "app-name" {
+    type = "string"
+    default = "APPDIRECTORY-dev"
+}
+
+variable "tags" {
+    type = "map"
+    default {
+        Environment = "Dev"
+    }
+}
+
+
+resource "azurerm_resource_group" "group" {
+    name = "${var.app-name}"
+    location = "ukwest"
+    tags = "${var.tags}"
+}
+
+resource "azurerm_storage_account" "storage" {
+    name = "${replace(var.app-name, "-", "")}storage"
+    resource_group_name = "${azurerm_resource_group.group.name}"
+    location = "${azurerm_resource_group.group.location}"
+    account_tier = "Standard"
+    account_replication_type = "RAGRS"
+    enable_blob_encryption = true
+
+    tags = "${var.tags}"
+}
