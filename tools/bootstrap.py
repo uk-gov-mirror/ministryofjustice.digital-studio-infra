4e16f9ae12fe8fb32a8a81d8db3fe35f4bad87c7
Simon Cross <simon.cross@digital.justice.gov.uk>
2018-02-16 17:28:21 +0000

    Added ability to create multiple environemnts

--- tools/bootstrap.py
+++ tools/bootstrap.py
@@ -13,27 +13,33 @@ parser = argparse.ArgumentParser(
     description='Script to create a basic app directory')
 
 parser.add_argument("-a", "--app-name", help="App name")
+parser.add_argument("-e", "--environments", help="Environments: comma separated list of dev,stage,mock,testing,preprod,prod")
 
 args = parser.parse_args()
 
-def create_app_boilerplate(app_directory):
+def create_app_boilerplate(app_directory,environments):
 
-    app_dev_directory = app_directory + "/dev"
+    for environment in environments:
+        app_dev_directory = app_directory + "/" + environment
+        app_name =  app_directory + "-" + environment
 
-    pathlib.Path(app_dev_directory).mkdir(parents=True)
+        pathlib.Path(app_dev_directory).mkdir(parents=True)
 
-    src = "tools/config/main.tf"
+        src = "tools/config/main.tf"
 
-    main_terraform = app_dev_directory + "/main.tf"
+        main_terraform = app_dev_directory + "/main.tf"
 
-    s = open(src).read()
-    s = s.replace('APPDIRECTORY', app_directory)
-    f = open(main_terraform, 'w')
-    f.write(s)
-    f.close()
+        s = open(src).read()
+        s = s.replace('APPNAME', app_name)
+        f = open(main_terraform, 'w')
+        f.write(s)
+        f.close()
 
 if not os.path.isdir(args.app_name):
-    create_app_boilerplate(args.app_name)
+
+    environments = args.environments.split(",")
+
+    create_app_boilerplate(args.app_name,environments)
 
 else:
     logging.warn("App directory already exists")
