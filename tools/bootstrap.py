b0d41058cacf4c8bc926951fcfa6f8ee7ffff4c6
Simon Cross <simon.cross@digital.justice.gov.uk>
2018-02-21 12:58:21 +0000

    Key vault added and symlink to common variables

--- tools/bootstrap.py
+++ tools/bootstrap.py
@@ -7,13 +7,15 @@ import logging
 import pathlib
 import shutil
 
+from python_modules import azure_account
+
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 parser = argparse.ArgumentParser(
     description='Script to create a basic app directory')
 
 parser.add_argument("-a", "--app-name", help="App name")
-parser.add_argument("-e", "--environments", help="Environments: comma separated list of dev,stage,mock,testing,preprod,prod")
+parser.add_argument("-e", "--environments", help="Environments: comma separated list of dev,stage,mock,preprod,prod")
 
 args = parser.parse_args()
 
@@ -25,6 +27,14 @@ def create_app_boilerplate(app_name,environments):
         storage_account = app_name.replace('-','') + environment + "storage"
 
         pathlib.Path(app_dev_directory).mkdir(parents=True)
+
+        prod_envs = ['prepod','prod']
+
+        if environment in prod_envs:
+            os.symlink(azure_account.get_git_root() + "/azure-prod.tf",app_dev_directory + '/azure-prod.tf')
+        else:
+            os.symlink(azure_account.get_git_root() + "/azure-devtest.tf",app_dev_directory + '/azure-devtest.tf')
+
 
         src = "tools/config/main.tf"
 
