2ac118e7963ea732b9be9808f4533b6bc04c14c4
Glen Mailer <glen@stainlessed.co.uk>
2018-03-05 13:49:29 +0000

    Update webops/dev so it still works

--- webops/dev/fixandgo.tf
+++ webops/dev/fixandgo.tf
@@ -1,37 +1,40 @@
 resource "azurerm_template_deployment" "fng-collection" {
-    name = "fng-jobs"
-    resource_group_name = "${azurerm_resource_group.group.name}"
-    deployment_mode = "Incremental"
-    template_body = "${file("../../shared/scheduler-collection.template.json")}"
-    parameters {
-        name = "fng-jobs"
-        service = "${var.tags["Service"]}"
-        environment = "${var.tags["Environment"]}"
-    }
+  name                = "fng-jobs"
+  resource_group_name = "${azurerm_resource_group.group.name}"
+  deployment_mode     = "Incremental"
+  template_body       = "${file("../../shared/scheduler-collection.template.json")}"
+
+  parameters {
+    name        = "fng-jobs"
+    service     = "${var.tags["Service"]}"
+    environment = "${var.tags["Environment"]}"
+  }
 }
 
 data "external" "fng-vault" {
-    program = ["node", "../../tools/keyvault-data-cli-auth.js"]
-    query {
-        vault = "${azurerm_key_vault.vault.name}"
+  program = ["python3", "../../tools/keyvault-data-cli-auth.py"]
 
-        oms_circle_token = "oms-circle-token"
-    }
+  query {
+    vault = "${azurerm_key_vault.vault.name}"
+
+    oms_circle_token = "oms-circle-token"
+  }
 }
 
 resource "azurerm_template_deployment" "fng-oms-computers" {
-    name = "fng-oms-computers"
-    resource_group_name = "${azurerm_resource_group.group.name}"
-    deployment_mode = "Incremental"
-    template_body = "${file("../../shared/scheduler-job.template.json")}"
-    parameters {
-        collection = "${azurerm_template_deployment.fng-collection.parameters.name}"
-        name = "fng-oms-computers"
-        service = "${var.tags["Service"]}"
-        environment = "${var.tags["Environment"]}"
-        uri = "https://circleci.com/api/v1.1/project/github/ministryofjustice/hmpps-azure-oms/tree/master"
-        body = "build_parameters[CIRCLE_JOB]=report"
-        contentType = "application/x-www-form-urlencoded"
-        authorization = "Basic ${base64encode("${data.external.fng-vault.result.oms_circle_token}:")}"
-    }
+  name                = "fng-oms-computers"
+  resource_group_name = "${azurerm_resource_group.group.name}"
+  deployment_mode     = "Incremental"
+  template_body       = "${file("../../shared/scheduler-job.template.json")}"
+
+  parameters {
+    collection    = "${azurerm_template_deployment.fng-collection.parameters.name}"
+    name          = "fng-oms-computers"
+    service       = "${var.tags["Service"]}"
+    environment   = "${var.tags["Environment"]}"
+    uri           = "https://circleci.com/api/v1.1/project/github/ministryofjustice/hmpps-azure-oms/tree/master"
+    body          = "build_parameters[CIRCLE_JOB]=report"
+    contentType   = "application/x-www-form-urlencoded"
+    authorization = "Basic ${base64encode("${data.external.fng-vault.result.oms_circle_token}:")}"
+  }
 }
