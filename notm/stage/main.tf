d58cd64f04766d60d26bbd028382875796635a85
Michael Willis <michael.willis@digital.justice.gov.uk>
2018-03-14 08:58:01 +0000

    NOTM dev / stage changes

--- notm/stage/main.tf
+++ notm/stage/main.tf
@@ -165,7 +165,7 @@ resource "azurerm_template_deployment" "webapp-config" {
         name = "${var.app-name}"
         APPINSIGHTS_INSTRUMENTATIONKEY = "${azurerm_template_deployment.insights.outputs["instrumentationKey"]}"
         NODE_ENV = "production"
-        API_ENDPOINT_URL = "https://gateway.t3.nomis-api.hmpps.dsd.io/elite2api/"
+        API_ENDPOINT_URL = "https://gateway.t2.nomis-api.hmpps.dsd.io/elite2api/"
         USE_API_GATEWAY_AUTH = "yes"
         NOMS_TOKEN = "${data.external.vault.result.noms_token}"
         NOMS_PRIVATE_KEY = "${data.external.vault.result.noms_private_key}"
