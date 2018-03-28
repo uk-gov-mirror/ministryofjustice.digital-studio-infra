337e0dd39f6b668f08b3f04bcebb5b4b987cd89d
Michael Willis <michael.willis@digital.justice.gov.uk>
2018-03-28 16:10:34 +0100

    keyworker service now has public jwt verifier key

--- keyworker-api/dev/vault-store.tf
+++ keyworker-api/dev/vault-store.tf
@@ -3,8 +3,8 @@ data "aws_ssm_parameter" "api-gateway-token" {
     name  = "/keyworker-api/dev/api_gateway_token"
 }
 
-data "aws_ssm_parameter" "jwt-signing-key" {
-    name  = "/keyworker-api/dev/jwt_signing_key"
+data "aws_ssm_parameter" "jwt-public-key" {
+    name  = "/keyworker-api/dev/jwt_public_key"
 }
 
 data "aws_ssm_parameter" "api-gateway-private-key" {
