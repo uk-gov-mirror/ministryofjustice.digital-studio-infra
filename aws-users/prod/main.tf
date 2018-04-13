e5903f2c36045d240e8962443cb9881ac702502d
Simon Cross <simon.cross@digital.justice.gov.uk>
2018-04-13 15:06:41 +0100

    Removed unnecsary files. Formatted to terraform standards.

--- aws-users/prod/main.tf
+++ aws-users/prod/main.tf
@@ -1,6 +1,6 @@
 variable "gpg_key" {
-    type = "string"
-    default = ""
+  type    = "string"
+  default = ""
 }
 
 resource "aws_iam_group" "webops" {
@@ -12,56 +12,56 @@ resource "aws_iam_group" "developers" {
 }
 
 data "external" "vault" {
-    program = ["python3", "../get-users.py"]
-    query {
-        vault = "${var.vault-name}"
-        users-webops = "users-webops"
-        users-developers = "users-developers"
-    }
+  program = ["python3", "../../tools/keyvault-data-cli-auth.py"]
+
+  query {
+    vault            = "${var.vault-name}"
+    users-webops     = "users-webops"
+    users-developers = "users-developers"
+  }
 }
 
 locals {
-aws_users_webops = "${compact(split(",",data.external.vault.result.users-webops))}"
-aws_users_developers = "${compact(split(",",data.external.vault.result.users-developers))}"
-
+  aws_users_webops     = "${compact(split(",",data.external.vault.result.users-webops))}"
+  aws_users_developers = "${compact(split(",",data.external.vault.result.users-developers))}"
 }
 
 resource "aws_iam_user" "user" {
   count = "${length(local.aws_users_webops)}"
-  name = "${element(local.aws_users_webops, count.index)}"
+  name  = "${element(local.aws_users_webops, count.index)}"
 }
 
 resource "aws_iam_user" "user-developer" {
   count = "${length(local.aws_users_developers)}"
-  name = "${element(local.aws_users_developers, count.index)}"
+  name  = "${element(local.aws_users_developers, count.index)}"
 }
 
 resource "aws_iam_group_membership" "webops_group_membership" {
-  name = "webops-group-membership"
+  name  = "webops-group-membership"
   users = ["${aws_iam_user.user.*.name}"]
   group = "${aws_iam_group.webops.name}"
 }
 
 resource "aws_iam_group_membership" "developers_group_membership" {
-  name = "developers-group-membership"
+  name  = "developers-group-membership"
   users = ["${aws_iam_user.user-developer.*.name}"]
   group = "${aws_iam_group.developers.name}"
 }
 
 locals {
-all_users = "${concat(local.aws_users_webops,local.aws_users_developers)}"
+  all_users = "${concat(local.aws_users_webops,local.aws_users_developers)}"
 }
 
 resource "aws_iam_user_login_profile" "user" {
-  count = "${length(local.all_users)}"
-  user    = "${element(local.all_users, count.index)}"
-  pgp_key = "${file(var.gpg_key)}"
+  count           = "${length(local.all_users)}"
+  user            = "${element(local.all_users, count.index)}"
+  pgp_key         = "${file(var.gpg_key)}"
   password_length = 14
 }
 
 data "template_file" "enable_mfa_policy" {
-
   template = "${file("../policies/enable-mfa-policy.json")}"
+
   vars {
     aws_account_id = "${var.aws_account_id}"
   }
