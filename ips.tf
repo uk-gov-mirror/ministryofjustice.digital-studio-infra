e7396c6a2d81f6e018046c66e42517b0cde127af
Simon Cross <simon.cross@digital.justice.gov.uk>
2018-04-20 09:14:52 +0100

    Prod/Preprod build

--- ips.tf
+++ ips.tf
@@ -1,11 +1,12 @@
 variable "ips" {
-    type = "map"
-    default {
-        office = "217.33.148.210"
-        quantum = "62.25.109.197"
-        health-kick = "35.177.252.195"
-        digitalprisons1 = "52.56.112.98"
-        digitalprisons2 = "52.56.118.154"
-        mojvpn = "81.134.202.29"
-    }
+  type = "map"
+
+  default {
+    office          = "217.33.148.210"
+    quantum         = "62.25.109.197"
+    health-kick     = "35.177.252.195"
+    digitalprisons1 = "52.56.112.98"
+    digitalprisons2 = "52.56.118.154"
+    mojvpn          = "81.134.202.29"
+  }
 }
