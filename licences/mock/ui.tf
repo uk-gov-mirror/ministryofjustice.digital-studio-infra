71cc865a8d1881c1c463239db9f29af3c16b005e
Michael Willis <michael.willis@digital.justice.gov.uk>
2018-05-01 16:50:08 +0100

    FIx CNAME

--- licences/mock/ui.tf
+++ licences/mock/ui.tf
@@ -50,22 +50,6 @@ data "external" "vault" {
   }
 }
 
-resource "azurerm_template_deployment" "ui-ssl" {
-  name                = "ui-ssl"
-  resource_group_name = "${azurerm_resource_group.group.name}"
-  deployment_mode     = "Incremental"
-  template_body       = "${file("../../shared/appservice-ssl.template.json")}"
-
-  parameters {
-    name             = "${azurerm_app_service.ui.name}"
-    hostname         = "${azurerm_dns_cname_record.ui.name}.${azurerm_dns_cname_record.ui.zone_name}"
-    keyVaultId       = "${azurerm_key_vault.vault.id}"
-    keyVaultCertName = "${replace("${azurerm_dns_cname_record.ui.name}.${azurerm_dns_cname_record.ui.zone_name}", ".", "DOT")}"
-    service          = "${var.tags["Service"]}"
-    environment      = "${var.tags["Environment"]}"
-  }
-}
-
 resource "azurerm_template_deployment" "ui-github" {
   name                = "ui-github"
   resource_group_name = "${azurerm_resource_group.group.name}"
@@ -111,13 +95,4 @@ module "slackhook-ui" {
   source   = "../../shared/modules/slackhook"
   app_name = "${azurerm_app_service.ui.name}"
   channels = ["licences-dev"]
-}
-
-resource "azurerm_dns_cname_record" "ui" {
-  name                = "licences-mock"
-  zone_name           = "hmpps.dsd.io"
-  resource_group_name = "webops"
-  ttl                 = "300"
-  record              = "${var.ui-name}.azurewebsites.net"
-  tags                = "${var.tags}"
 }
--- licences/stage/ui.tf
+++ licences/stage/ui.tf
@@ -30,7 +30,7 @@ resource "azurerm_app_service" "ui" {
     DB_PASS           = "${random_id.sql-ui-password.b64}"
     DB_SERVER         = "${module.sql.db_server}"
     DB_NAME           = "${module.sql.db_name}"
-    NOMIS_API_URL     = "https://noms-api-dev.dsd.io/elite2api"
+    NOMIS_API_URL     = "https://noms-api-dev.dsd.io/elite2api/api"
     ENABLE_TEST_UTILS = true
 
     APPINSIGHTS_INSTRUMENTATIONKEY = "${azurerm_application_insights.insights.instrumentation_key}"
@@ -51,21 +51,6 @@ data "external" "vault" {
   }
 }
 
-resource "azurerm_template_deployment" "ui-ssl" {
-  name                = "ui-ssl"
-  resource_group_name = "${azurerm_resource_group.group.name}"
-  deployment_mode     = "Incremental"
-  template_body       = "${file("../../shared/appservice-ssl.template.json")}"
-
-  parameters {
-    name             = "${azurerm_app_service.ui.name}"
-    hostname         = "${azurerm_dns_cname_record.ui.name}.${azurerm_dns_cname_record.ui.zone_name}"
-    keyVaultId       = "${azurerm_key_vault.vault.id}"
-    keyVaultCertName = "${replace("${azurerm_dns_cname_record.ui.name}.${azurerm_dns_cname_record.ui.zone_name}", ".", "DOT")}"
-    service          = "${var.tags["Service"]}"
-    environment      = "${var.tags["Environment"]}"
-  }
-}
 
 resource "azurerm_template_deployment" "ui-github" {
   name                = "ui-github"
@@ -112,13 +97,4 @@ module "slackhook-ui" {
   source   = "../../shared/modules/slackhook"
   app_name = "${azurerm_app_service.ui.name}"
   channels = ["licences-dev"]
-}
-
-resource "azurerm_dns_cname_record" "ui" {
-  name                = "licences-stage"
-  zone_name           = "hmpps.dsd.io"
-  resource_group_name = "webops"
-  ttl                 = "300"
-  record              = "${var.ui-name}.azurewebsites.net"
-  tags                = "${var.tags}"
 }
