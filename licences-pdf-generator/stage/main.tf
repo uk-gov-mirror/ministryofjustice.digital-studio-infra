34a5a1d71336f447f5fba15554e2c123b187deee
Alistair Todd <alistair.todd@digital.justice.gov.uk>
2018-05-22 12:30:17 +0100

    Licences PDF generator

---
+++ licences-pdf-generator/stage/main.tf
@@ -0,0 +1,187 @@
+variable "app-name" {
+  type    = "string"
+  default = "licences-pdf-generator"
+}
+
+variable "tags" {
+  type = "map"
+
+  default {
+    Service     = "licences-pdf-generator"
+    Environment = "stage"
+  }
+}
+
+# This resource is managed in multiple places (omic ui stage)
+resource "aws_elastic_beanstalk_application" "app" {
+  name        = "${var.app-name}"
+  description = "${var.app-name}"
+}
+
+resource "random_id" "session-secret" {
+  byte_length = 40
+}
+
+resource "azurerm_resource_group" "group" {
+  name     = "${var.app-name}"
+  location = "ukwest"
+  tags     = "${var.tags}"
+}
+
+data "aws_acm_certificate" "cert" {
+  domain = "${var.app-name}.hmpps.dsd.io"
+}
+
+data "aws_elastic_beanstalk_solution_stack" "docker" {
+  most_recent = true
+  name_regex  = "^64bit Amazon Linux .* v2.* running Docker 17.*$"
+}
+
+resource "aws_elastic_beanstalk_environment" "app-env" {
+  name                = "${var.app-name}"
+  application         = "${aws_elastic_beanstalk_application.app.name}"
+  solution_stack_name = "${data.aws_elastic_beanstalk_solution_stack.docker.name}"
+  tier                = "WebServer"
+
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "InstanceType"
+    value     = "t2.micro"
+  }
+
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "IamInstanceProfile"
+    value     = "aws-elasticbeanstalk-ec2-role"
+  }
+
+  setting {
+    namespace = "aws:elasticbeanstalk:application"
+    name      = "Application Healthcheck URL"
+    value     = "/health"
+  }
+
+  setting {
+    namespace = "aws:elasticbeanstalk:environment"
+    name      = "ServiceRole"
+    value     = "aws-elasticbeanstalk-service-role"
+  }
+
+  setting {
+    namespace = "aws:elb:listener:443"
+    name      = "ListenerProtocol"
+    value     = "HTTPS"
+  }
+
+  setting {
+    namespace = "aws:elb:listener:443"
+    name      = "SSLCertificateId"
+    value     = "${data.aws_acm_certificate.cert.arn}"
+  }
+
+  setting {
+    namespace = "aws:elb:listener:443"
+    name      = "InstancePort"
+    value     = "80"
+  }
+
+  setting {
+    namespace = "aws:elb:listener:443"
+    name      = "ListenerProtocol"
+    value     = "HTTPS"
+  }
+
+  setting {
+    namespace = "aws:elb:policies:tlshigh"
+    name      = "LoadBalancerPorts"
+    value     = "443"
+  }
+
+  setting {
+    namespace = "aws:elb:policies:tlshigh"
+    name      = "SSLReferencePolicy"
+    value     = "ELBSecurityPolicy-TLS-1-2-2017-01"
+  }
+
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "VPCId"
+    value     = "${aws_vpc.vpc.id}"
+  }
+
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "Subnets"
+    value     = "${aws_subnet.private-a.id}"
+  }
+
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "ELBSubnets"
+    value     = "${aws_subnet.public-a.id}"
+  }
+
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "AssociatePublicIpAddress"
+    value     = "false"
+  }
+
+  setting {
+    namespace = "aws:elasticbeanstalk:healthreporting:system"
+    name      = "SystemType"
+    value     = "enhanced"
+  }
+
+  setting {
+    namespace = "aws:elasticbeanstalk:managedactions"
+    name      = "ManagedActionsEnabled"
+    value     = "true"
+  }
+
+  setting {
+    namespace = "aws:elasticbeanstalk:managedactions"
+    name      = "PreferredStartTime"
+    value     = "Fri:10:00"
+  }
+
+  setting {
+    namespace = "aws:elasticbeanstalk:managedactions:platformupdate"
+    name      = "UpdateLevel"
+    value     = "minor"
+  }
+
+  setting {
+    namespace = "aws:elasticbeanstalk:managedactions:platformupdate"
+    name      = "InstanceRefreshEnabled"
+    value     = "true"
+  }
+
+  setting {
+    namespace = "aws:elasticbeanstalk:cloudwatch:logs"
+    name      = "StreamLogs"
+    value     = "true"
+  }
+
+  # Begin app-specific config settings
+
+
+  tags = "${var.tags}"
+}
+
+resource "azurerm_dns_cname_record" "cname" {
+  name                = "${var.app-name}"
+  zone_name           = "hmpps.dsd.io"
+  resource_group_name = "webops"
+  ttl                 = "60"
+  record              = "${aws_elastic_beanstalk_environment.app-env.cname}"
+}
+
+# Allow AWS's ACM to manage omic-dev.hmpps.dsd.io
+resource "azurerm_dns_cname_record" "acm-verify" {
+  name                = "_79224e76e3cf7223cd35155455755acc.omic-dev"
+  zone_name           = "hmpps.dsd.io"
+  resource_group_name = "webops"
+  ttl                 = "300"
+  record              = "_3f84403eebcf649218bc22a4654a5fa4.acm-validations.aws."
+}
