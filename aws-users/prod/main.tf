9011f819198afd4d83f06dd253db512919ab2731
Simon Cross <simon.cross@digital.justice.gov.uk>
2018-06-18 09:37:37 +0100

    Restrict devs to their own elasticbeanstalk apps

--- aws-users/prod/main.tf
+++ aws-users/prod/main.tf
@@ -1,3 +1,20 @@
+variable "newnomis-apps" {
+  default = [
+    "arn:aws:elasticbeanstalk:eu-west-2:589133037702:application/keyworker-api",
+    "arn:aws:elasticbeanstalk:eu-west-2:589133037702:application/omic-ui",
+    "arn:aws:elasticbeanstalk:eu-west-2:589133037702:applicationversion/keyworker-api/*",
+    "arn:aws:elasticbeanstalk:eu-west-2:589133037702:applicationversion/omic-ui/*",
+    "arn:aws:elasticbeanstalk:eu-west-2:589133037702:configurationtemplate/keyworker-api/*",
+    "arn:aws:elasticbeanstalk:eu-west-2:589133037702:configurationtemplate/omic-ui/*",
+    "arn:aws:elasticbeanstalk:eu-west-2:589133037702:environment/keyworker-api/*",
+    "arn:aws:elasticbeanstalk:eu-west-2:589133037702:environment/omic-ui/*"
+  ]
+}
+
+locals {
+  newnomis_apps_list = "${join("\",\"",var.newnomis-apps)}"
+}
+
 resource "aws_iam_account_password_policy" "strict" {
   minimum_password_length        = 14
   require_lowercase_characters   = true
@@ -11,25 +28,25 @@ resource "aws_iam_group" "webops" {
   name = "WebOps"
 }
 
-resource "aws_iam_group" "developers" {
-  name = "Developers"
+resource "aws_iam_group" "newnomis-developers" {
+  name = "NewNOMIS-Devs"
 }
 
 data "external" "vault" {
   program = ["python3", "../../tools/keyvault-data-cli-auth.py"]
 
   query {
-    vault            = "${var.vault-name}"
-    users-webops     = "webops"
-    users-developers = "developers"
+    vault                     = "${var.vault-name}"
+    users-webops              = "webops"
+    users-newnomis-developers = "newnomis-developers"
   }
 }
 
 locals {
-  aws_users_webops     = "${compact(split(",",data.external.vault.result.users-webops))}"
-  aws_users_developers = "${compact(split(",",data.external.vault.result.users-developers))}"
+  aws_users_webops              = "${compact(split(",",data.external.vault.result.users-webops))}"
+  aws_users_newnomis_developers = "${compact(split(",",data.external.vault.result.users-newnomis-developers))}"
 
-  all_users = "${concat(local.aws_users_webops,local.aws_users_developers)}"
+  all_users = "${concat(local.aws_users_webops,local.aws_users_newnomis_developers)}"
 }
 
 resource "aws_iam_user" "user" {
@@ -43,7 +60,7 @@ resource "aws_iam_user" "user" {
     password=$(pwgen 20 1)
     echo Initial Password: "$password"
     aws iam create-login-profile \
-      --user-name "${element(local.aws_users_webops, count.index)}" \
+      --user-name "${element(local.all_users, count.index)}" \
       --password "$password" \
       --password-reset-required
 SHELL
@@ -59,10 +76,10 @@ resource "aws_iam_group_membership" "webops_group_membership" {
   group = "${aws_iam_group.webops.name}"
 }
 
-resource "aws_iam_group_membership" "developers_group_membership" {
-  name  = "developers-group-membership"
-  users = ["${local.aws_users_developers}"]
-  group = "${aws_iam_group.developers.name}"
+resource "aws_iam_group_membership" "newnomis_developers_group_membership" {
+  name  = "newnomis-developers-group-membership"
+  users = ["${local.aws_users_newnomis_developers}"]
+  group = "${aws_iam_group.newnomis-developers.name}"
 }
 
 data "template_file" "enable_mfa_policy" {
@@ -73,10 +90,25 @@ data "template_file" "enable_mfa_policy" {
   }
 }
 
+data "template_file" "devs-elasticbeanstalk-deploy-policy" {
+  template = "${file("../policies/devs-elasticbeanstalk-deploy-policy.json")}"
+
+  vars {
+    aws_account_id       = "${var.aws_account_id}"
+    aws_application_name = "${local.newnomis_apps_list}"
+  }
+}
+
 resource "aws_iam_policy" "enable-mfa" {
   name        = "enable-mfa"
   description = "Enable MFA on user accounts"
   policy      = "${data.template_file.enable_mfa_policy.rendered}"
+}
+
+resource "aws_iam_policy" "devs-elasticbeanstalk-deploy" {
+  name        = "devs-elasticbeanstalk-deploy"
+  description = "Allow devs to deploy Elasticbeanstalk apps"
+  policy      = "${data.template_file.devs-elasticbeanstalk-deploy-policy.rendered}"
 }
 
 resource "aws_iam_group_policy_attachment" "webops-attach-enable-mfa" {
@@ -84,9 +116,14 @@ resource "aws_iam_group_policy_attachment" "webops-attach-enable-mfa" {
   policy_arn = "${aws_iam_policy.enable-mfa.arn}"
 }
 
-resource "aws_iam_group_policy_attachment" "developers-attach-enable-mfa" {
-  group      = "${aws_iam_group.developers.name}"
+resource "aws_iam_group_policy_attachment" "newnomis-developers-attach-enable-mfa" {
+  group      = "${aws_iam_group.newnomis-developers.name}"
   policy_arn = "${aws_iam_policy.enable-mfa.arn}"
+}
+
+resource "aws_iam_group_policy_attachment" "devs-elasticbeanstalk-deploy" {
+  group      = "${aws_iam_group.newnomis-developers.name}"
+  policy_arn = "${aws_iam_policy.devs-elasticbeanstalk-deploy.arn}"
 }
 
 resource "aws_iam_group_policy_attachment" "webops-attach-iam-password-change" {
@@ -95,7 +132,7 @@ resource "aws_iam_group_policy_attachment" "webops-attach-iam-password-change" {
 }
 
 resource "aws_iam_group_policy_attachment" "developers-attach-iam-password-change" {
-  group      = "${aws_iam_group.developers.name}"
+  group      = "${aws_iam_group.newnomis-developers.name}"
   policy_arn = "arn:aws:iam::aws:policy/IAMUserChangePassword"
 }
 
