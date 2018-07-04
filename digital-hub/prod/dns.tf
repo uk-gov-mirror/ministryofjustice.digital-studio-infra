6a702ad6756ad7047c14e94e80637339468ffc06
Glen Mailer <glen@stainlessed.co.uk>
2018-07-04 17:40:53 +0100

    Import hub bounce box DNS entry

--- digital-hub/prod/dns.tf
+++ digital-hub/prod/dns.tf
@@ -1,15 +1,23 @@
+resource "azurerm_dns_a_record" "admin" {
+  name                = "prod.admin.hub"
+  zone_name           = "service.hmpps.dsd.io"
+  resource_group_name = "webops-prod"
+  ttl                 = "300"
+  records             = ["${azurerm_public_ip.hub-bounce-prod-ip.ip_address}"]
+}
+
 resource "azurerm_dns_cname_record" "cname_berwyn" {
-    name = "bli.prod.admin.hub"
-    zone_name = "service.hmpps.dsd.io"
-    resource_group_name = "webops-prod"
-    ttl = "300"
-    record = "prod.admin.hub.service.hmpps.dsd.io"
+  name                = "bli.prod.admin.hub"
+  zone_name           = "service.hmpps.dsd.io"
+  resource_group_name = "webops-prod"
+  ttl                 = "300"
+  record              = "prod.admin.hub.service.hmpps.dsd.io"
 }
 
 resource "azurerm_dns_cname_record" "cname_wayland" {
-    name = "wli.prod.admin.hub"
-    zone_name = "service.hmpps.dsd.io"
-    resource_group_name = "webops-prod"
-    ttl = "300"
-    record = "prod.admin.hub.service.hmpps.dsd.io"
+  name                = "wli.prod.admin.hub"
+  zone_name           = "service.hmpps.dsd.io"
+  resource_group_name = "webops-prod"
+  ttl                 = "300"
+  record              = "prod.admin.hub.service.hmpps.dsd.io"
 }
