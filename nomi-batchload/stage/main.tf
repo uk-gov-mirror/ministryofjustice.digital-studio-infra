e3f303ca6f6540ba2e66e991cd5d67aea5178f32
Alistair Todd <alistair.todd@digital.justice.gov.uk>
2018-07-05 13:50:00 +0100

    Nomis Batchloader

---
+++ nomi-batchload/stage/main.tf
@@ -0,0 +1,85 @@
+resource "azurerm_resource_group" "group" {
+  name     = "${var.app-name}"
+  location = "ukwest"
+  tags     = "${var.tags}"
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "${replace(var.app-name, "-", "")}storage"
+  resource_group_name      = "${azurerm_resource_group.group.name}"
+  location                 = "${azurerm_resource_group.group.location}"
+  account_tier             = "Standard"
+  account_replication_type = "RAGRS"
+  enable_blob_encryption   = true
+
+  tags = "${var.tags}"
+}
+
+resource "azurerm_storage_container" "logs" {
+  name                  = "web-logs"
+  resource_group_name   = "${azurerm_resource_group.group.name}"
+  storage_account_name  = "${azurerm_storage_account.storage.name}"
+  container_access_type = "private"
+}
+
+resource "azurerm_key_vault" "vault" {
+  name                = "${var.app-name}"
+  resource_group_name = "${azurerm_resource_group.group.name}"
+  location            = "${azurerm_resource_group.group.location}"
+
+  sku {
+    name = "standard"
+  }
+
+  tenant_id = "${var.azure_tenant_id}"
+
+  access_policy {
+    tenant_id          = "${var.azure_tenant_id}"
+    object_id          = "${var.azure_webops_group_oid}"
+    key_permissions    = []
+    secret_permissions = "${var.azure_secret_permissions_all}"
+  }
+
+  access_policy {
+    tenant_id          = "${var.azure_tenant_id}"
+    object_id          = "${var.azure_app_service_oid}"
+    key_permissions    = []
+    secret_permissions = ["get"]
+  }
+
+  access_policy {
+    tenant_id          = "${var.azure_tenant_id}"
+    object_id          = "${var.azure_jenkins_sp_oid}"
+    key_permissions    = []
+    secret_permissions = ["set"]
+  }
+
+  access_policy {
+    tenant_id          = "${var.azure_tenant_id}"
+    object_id          = "${var.azure_licences_group_oid}"
+    key_permissions    = []
+    secret_permissions = "${var.azure_secret_permissions_all}"
+  }
+
+  enabled_for_deployment          = false
+  enabled_for_disk_encryption     = false
+  enabled_for_template_deployment = true
+
+  tags = "${var.tags}"
+}
+
+data "aws_ssm_parameter" "api_gateway_token" {
+  name = "/nomis-batchload/stage/api_gateway_token"
+}
+
+data "aws_ssm_parameter" "api_gateway_private_key" {
+  name = "/nomis-batchload/stage/api_gateway_private_key"
+}
+
+data "aws_ssm_parameter" "api_client_secret" {
+  name = "/nomis-batchload/stage/api_client_secret"
+}
+
+data "aws_ssm_parameter" "admin_api_client_secret" {
+  name = "/nomis-batchload/stage/admin_api_client_secret"
+}
