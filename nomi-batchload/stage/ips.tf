cbf2098e95901eb485280df4c9b5648d38a2bdfe
Alistair Todd <alistair.todd@digital.justice.gov.uk>
2018-07-05 13:50:00 +0100

    Nomis Batchloader

---
+++ nomi-batchload/stage/ips.tf
@@ -0,0 +1,13 @@
+variable "ips" {
+  type = "map"
+
+  default {
+    office          = "217.33.148.210"
+    quantum         = "62.25.109.197"
+    health-kick     = "35.177.252.195"
+    digitalprisons1 = "52.56.112.98"
+    digitalprisons2 = "52.56.118.154"
+    dxc             = "109.147.195.53"
+    mojvpn          = "81.134.202.29"
+  }
+}
