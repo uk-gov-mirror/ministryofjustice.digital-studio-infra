b96fbb59b5235c32e1c9a9a6a93cdd48bec8313d
Matt Whittaker <matt.whittaker@digital.justice.gov.uk>
2018-08-22 11:09:03 +0100

    support for wildcard certs

--- tools/letsencrypt/authenticator.py
+++ tools/letsencrypt/authenticator.py
@@ -15,42 +15,43 @@ for key,value in dns_names.items():
 
     host = value[0]
     zone = value[1]
-
-    domain = host + "." + zone
-
-    if os.getenv("CERTBOT_DOMAIN") == domain:
-
+    
+    logging.info("host and zone =  %s.%s" % (host, zone))
+    
+    if host == "*":
+        acme_challenge_name = "_acme-challenge"
+    else:
         acme_challenge_name = "_acme-challenge." + host
 
-        create_record_set = subprocess.run(
-            ["az", "network", "dns", "record-set", "txt", "create",
-             "--name", acme_challenge_name,
+    create_record_set = subprocess.run(
+        ["az", "network", "dns", "record-set", "txt", "create",
+         "--name", acme_challenge_name,
+         "--resource-group", resource_group,
+         "--zone-name", zone,
+         "--ttl", "60"
+         ],
+        stdout=subprocess.PIPE,
+        check=True
+    )
+
+    if create_record_set.returncode == 0:
+
+        logging.info("Created DNS record set for %s.%s" % (host, zone))
+
+        create_dns_record = subprocess.run(
+            ["az", "network", "dns", "record-set", "txt", "add-record",
+             "--record-set-name", acme_challenge_name,
              "--resource-group", resource_group,
              "--zone-name", zone,
-             "--ttl", "60"
+             "--value", os.getenv("CERTBOT_VALIDATION")
              ],
             stdout=subprocess.PIPE,
             check=True
         )
-
-        if create_record_set.returncode == 0:
-
-            logging.info("Created DNS record set for %s.%s" % (host, zone))
-
-            create_dns_record = subprocess.run(
-                ["az", "network", "dns", "record-set", "txt", "add-record",
-                 "--record-set-name", acme_challenge_name,
-                 "--resource-group", resource_group,
-                 "--zone-name", zone,
-                 "--value", os.getenv("CERTBOT_VALIDATION")
-                 ],
-                stdout=subprocess.PIPE,
-                check=True
-            )
-            if create_dns_record.returncode == 0:
-                logging.info("Created DNS record for %s.%s" % (host, zone))
-            else:
-                logging.warn("Error updating DNS record for %s.%s" % (host, zone))
+        if create_dns_record.returncode == 0:
+            logging.info("Created DNS record for %s.%s" % (host, zone))
         else:
-            logging.warn("Error creating DNS record set for %s.%s" %
-                         (host, zone))
+            logging.warn("Error updating DNS record for %s.%s" % (host, zone))
+    else:
+        logging.warn("Error creating DNS record set for %s.%s" %
+                     (host, zone))
