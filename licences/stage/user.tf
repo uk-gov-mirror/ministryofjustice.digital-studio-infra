2c7e536cede2d2dcf3f34c34c9cf65bb31b59871
Matt Whittaker <matt.whittaker@digital.justice.gov.uk>
2018-09-25 13:46:11 +0100

    fix permission for ci deployer

--- licences/stage/user.tf
+++ licences/stage/user.tf
@@ -92,11 +92,12 @@ resource "aws_iam_group_policy" "deployers" {
                 "elasticloadbalancing:DescribeLoadBalancers",
                 "elasticloadbalancing:DescribeTargetGroups",
                 "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
-                "elasticloadbalancing:DeregisterInstancesWithLoadBalancer"
+                "elasticloadbalancing:DeregisterInstancesWithLoadBalancer",
+                "elasticloadbalancing:RegisterTargets" 
             ],
             "Effect": "Allow",
             "Resource": [
-                "arn:aws:elasticloadbalancing:${var.aws_region}:${var.aws_account_id}:loadbalancer/*"
+                "*"
             ]
         },
         {
