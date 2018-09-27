508bc01292f7bb3b260c2b86a417de5bb05a15b2
Michael Willis <michael.willis@digital.justice.gov.uk>
2018-09-27 16:42:20 +0100

    Add new whitelistings for J5 phones and sodexo

--- ips.tf
+++ ips.tf
@@ -2,12 +2,15 @@ variable "ips" {
   type = "map"
 
   default {
-    office          = "217.33.148.210"
-    quantum         = "62.25.109.197"
-    health-kick     = "35.177.252.195"
-    digitalprisons1 = "52.56.112.98"
-    digitalprisons2 = "52.56.118.154"
-    dxc             = "109.147.195.53"
-    mojvpn          = "81.134.202.29"
+    office                = "217.33.148.210"
+    quantum               = "62.25.109.197"
+    health-kick           = "35.177.252.195"
+    digitalprisons1       = "52.56.112.98"
+    digitalprisons2       = "52.56.118.154"
+    dxc                   = "109.147.195.53"
+    mojvpn                = "81.134.202.29"
+    j5-phones-1           = "35.177.125.252"
+    j5-phones-2           = "35.177.137.160"
+    sodexo-northumberland = "88.98.48.10"
   }
 }
