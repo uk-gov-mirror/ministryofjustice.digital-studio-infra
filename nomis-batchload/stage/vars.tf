c5ecc017981dc6ea476153a500aa8aebda2eafd1
Matt Whittaker <matt.whittaker@digital.justice.gov.uk>
2018-10-01 11:26:52 +0100

    added DB backup and multi-az settings

--- nomis-batchload/stage/vars.tf
+++ nomis-batchload/stage/vars.tf
@@ -16,6 +16,10 @@ variable "tags" {
 locals {
   instances = "2"
   mininstances = "1"
+  db_multi_az = "false"
+  db_backup_retention_period = "0"
+  db_maintenance_window = "Mon:00:00-Sun:11:59"
+  db_apply_immediately = "true"
 }
 
 # App settings
