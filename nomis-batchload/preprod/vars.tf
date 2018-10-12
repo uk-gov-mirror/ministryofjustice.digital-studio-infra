d058023fbe46da6819326fcf29f64f165f651688
Peter Phillips <peter@greenthistle.co.uk>
2018-10-12 11:19:43 +0100

    NN-1200: Switch to direct access to auth rather than through elite2api

--- nomis-batchload/preprod/vars.tf
+++ nomis-batchload/preprod/vars.tf
@@ -25,7 +25,7 @@ locals {
 # App settings
 locals {
   nomis_api_url       = "https://gateway.preprod.nomis-api.service.hmpps.dsd.io/elite2api/api"
-  nomis_auth_url      = "https://gateway.preprod.nomis-api.service.hmpps.dsd.io/elite2api"
+  nomis_auth_url      = "https://gateway.preprod.nomis-api.service.hmpps.dsd.io/auth"
   api_client_id       = "batchadmin"
 }
 
