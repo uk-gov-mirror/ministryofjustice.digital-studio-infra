85bc1832bee201abf8d6b0e4f108995c2cef912d
Michael Willis <michael.willis@digital.justice.gov.uk>
2018-10-25 16:58:57 +0100

    Add All OMIC and NOTM env changes

--- notm/prod/main.tf
+++ notm/prod/main.tf
@@ -124,6 +124,7 @@ resource "azurerm_app_service" "app" {
     OMIC_UI_URL                    = "https://omic.service.hmpps.dsd.io/"
     WHEREABOUTS_UI_URL             = "https://prisonstaffhub.service.hmpps.dsd.io/whereaboutssearch"
     ESTABLISHMENT_ROLLCHECK_URL    = "https://prisonstaffhub.service.hmpps.dsd.io/establishmentroll"
+    PRISON_STAFF_HUB_UI_URL        = "https://prisonstaffhub.service.hmpps.dsd.io/"
     API_CLIENT_ID                  = "elite2apiclient"
     API_CLIENT_SECRET              = "${data.external.vault.result.api_client_secret}"
     GOOGLE_ANALYTICS_ID            = "${data.external.vault.result.google_analytics_id}"
