5e22072ace19a39e6f02bb55e806e744c62454a0
Jon Nield <7629656+Tiki-God@users.noreply.github.com>
2018-11-05 13:39:52 +0000

    Store usernames in SSM

--- nomis-batchload/preprod/vault-store.tf
+++ nomis-batchload/preprod/vault-store.tf
@@ -5,3 +5,7 @@ data "aws_ssm_parameter" "api-client-secret" {
 data "aws_ssm_parameter" "admin-api-client-secret" {
   name = "/${lower(replace(var.tags["Service"], " ", "-"))}/${lower(var.tags["Environment"])}/admin_api_client_secret"
 }
+
+data "aws_ssm_parameter" "developer-list" {
+  name = "developers"
+}
