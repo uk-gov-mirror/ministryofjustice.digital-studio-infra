2e7050b484342a5ff579852776b50691ca09672a
Jon Nield <7629656+Tiki-God@users.noreply.github.com>
2018-11-05 14:24:26 +0000

    Replicating live permissions

--- nomis-batchload/prod/user.tf
+++ nomis-batchload/prod/user.tf
@@ -93,12 +93,14 @@ resource "aws_iam_group_policy" "deployers" {
             "Action": [
                 "elasticloadbalancing:DescribeInstanceHealth",
                 "elasticloadbalancing:DescribeLoadBalancers",
+                "elasticloadbalancing:RegisterTargets",
                 "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
                 "elasticloadbalancing:DeregisterInstancesWithLoadBalancer"
             ],
             "Effect": "Allow",
             "Resource": [
-                "arn:aws:elasticloadbalancing:${var.aws_region}:${var.aws_account_id}:loadbalancer/awseb-*"
+                "arn:aws:elasticloadbalancing:${var.aws_region}:${var.aws_account_id}:loadbalancer/awseb-*",
+                "arn:aws:elasticloadbalancing:${var.aws_region}:${var.aws_account_id}:targetgroup/awseb-*"
             ]
         },
         {
