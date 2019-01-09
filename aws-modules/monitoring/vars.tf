bb3116c109bc37b4fe7aa0e428d69c0b025428a8
iobend3r <adam@arctec.io>
2019-01-09 13:56:35 +0000

    Fix allowed_inbound_ip to use CIDR

--- aws-modules/monitoring/vars.tf
+++ aws-modules/monitoring/vars.tf
@@ -8,7 +8,7 @@ locals {
   default_iam_resource_name_root = "dso-${local.default_environment_name}-cloudwatch-"
   default_vpc_ip_range = "192.168.0.0/24"
   default_subnet_ip_range = "192.168.0.0/28"
-  allowed_inbound_ip = "217.33.148.210"
+  allowed_inbound_ip = "217.33.148.210/32"
   default_ec2_instance_size = "t2.small"
   default_ec2_instance_private_ips = ["192.168.0.5"]
   default_ec2_instance_key_pair = "${var.key-pair-name}"
