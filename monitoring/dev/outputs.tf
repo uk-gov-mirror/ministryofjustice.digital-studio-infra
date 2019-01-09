0162338f248ce9e6805946332dfb4cc25ce9b923
iobend3r <adam@arctec.io>
2019-01-09 14:03:53 +0000

    Remove secret output from aws monitoring

--- monitoring/dev/outputs.tf
+++ monitoring/dev/outputs.tf
@@ -5,7 +5,3 @@ output "ec2_instance_ip" {
 output "ec2_instance_dns" {
   value = "${module.monitoring.ec2_instance_dns}"
 }
-
-output "ec2_instance_secret" {
-  value = "${module.monitoring.ec2_instance_secret}"
-}
