e20c9a3d530a5e5e3e81524161adba82c86919e2
iobend3r <adam@arctec.io>
2019-01-09 14:03:33 +0000

    Remove secret output from aws monitoring module

--- aws-modules/monitoring/outputs.tf
+++ aws-modules/monitoring/outputs.tf
@@ -5,7 +5,3 @@ output "ec2_instance_ip" {
 output "ec2_instance_dns" {
   value = "${aws_instance.monitoring_ec2_instance.public_dns}"
 }
-
-output "ec2_instance_secret" {
-  value = "${aws_iam_access_key.monitoring_iam_access_key.encrypted_secret}"
-}
