2155b49a25e92ab556f7d6babc031d77ab0da9be
iobend3r <adam@arctec.io>
2019-01-10 09:55:09 +0000

    Add required variables in monitoring app to generate tags

--- monitoring/dev/main.tf
+++ monitoring/dev/main.tf
@@ -2,9 +2,11 @@ module "monitoring" {
   source = "../../aws-modules/monitoring"
   environment-name = "${local.environment_name}"
   key-pair-name = "${local.key_pair_name}"
+  application-name = "${local.application_name}"
 }
 
 locals {
   environment_name = "${var.environment-name}"
   key_pair_name = "${var.key-pair-name}"
+  application_name = "${var.application-name}"
 }
--- monitoring/dev/vars.tf
+++ monitoring/dev/vars.tf
@@ -1,6 +1,9 @@
 variable "environment-name" {
     type = "string"
-    default = "dev"
+}
+
+variable "application-name" {
+  default = "monitoring"
 }
 
 variable "key-pair-name" {
