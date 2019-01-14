228bc37ea4e625c1836e7d2a79b9140937a69935
Matt <34448412+mattops@users.noreply.github.com>
2019-01-14 14:34:24 +0000

    fix for wildcard case (#278)
    * fix for wildcard case

--- tools/ssl-certs/application_gateway_update.py
+++ tools/ssl-certs/application_gateway_update.py
@@ -40,18 +40,21 @@ def update_ssl_cert(app_gateway_name, resource_group, gw_cert_name, key_vault):
 
     logging.info("Updating Application Gateway with new certificate.")
 
-    try:
-        set_secret = subprocess.run(
-            ["az", "network", "application-gateway", "ssl-cert", "update",
+    cmd = ["az", "network", "application-gateway", "ssl-cert", "update",
              "--gateway-name", app_gateway_name,
              "--resource-group", resource_group,
              "--name", gw_cert_name,
              "--cert-file", "appgw.pfx",
              "--cert-password", certificate["appgw-ssl-certificate-password"]
-             ],
+    ]
+    logging.info("Running: %s" % (" ".join(cmd)))
+    try:
+        set_secret = subprocess.run(
+            cmd,
             stdout=subprocess.PIPE,
             check=True
         )
+        logging.info(set_secret)
 
     except subprocess.CalledProcessError:
         sys.exit("There was an error updating the application gateway SSL certificate.")
