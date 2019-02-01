e9557fb51782434e9d1be2c193c1086b82480a71
--global <adam@arctec.io>
2019-02-01 15:14:32 +0000

    aws monitoring module add better outputs incl dns names and ips

--- aws-modules/monitoring/outputs.tf
+++ aws-modules/monitoring/outputs.tf
@@ -1,7 +1,7 @@
-output "ec2_instance_ip" {
-  value = "${aws_eip.monitoring_eip.public_ip}"
+output "monitoring_dns_urls" {
+  value = ["${aws_instance.monitoring_ec2_instance.public_dns}", "${azurerm_dns_a_record.monitoring_dns_a_record.name}.${azurerm_dns_a_record.monitoring_dns_a_record.zone_name}"]
 }
 
-output "ec2_instance_dns" {
-  value = "${aws_instance.monitoring_ec2_instance.public_dns}"
+output "monitoring_ip" {
+  value = "${aws_eip.monitoring_eip.public_ip}"
 }
