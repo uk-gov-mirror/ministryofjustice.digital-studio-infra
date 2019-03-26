57648ac081d9f86a13cd72699008c498b9959809
jhackettpps <34453152+jhackettpps@users.noreply.github.com>
2019-03-26 14:56:57 +0000

    Adding github ips to whitelist for devtest (#311)
    * Adding github ips to whitelist for devtest
    
    * Properly describing nsg rules
    
    * Altering prod nsg
    
    * fix slackhook webapp and NSG description
    
    * remove not needed template, added https only
    
    * Adding destination prefix
    
    * Specific IP for prod
    
    * added dest address for NSG rules

--- webops/prod/nsg-mgmt-subnet.tf
+++ webops/prod/nsg-mgmt-subnet.tf
@@ -1,3 +1,5 @@
+data "github_ip_ranges" "whitelist" {}
+
 resource "azurerm_network_security_group" "mgmt-app-gw" {
   name                = "mgmt-app-gw"
   location            = "ukwest"
@@ -31,6 +33,19 @@ resource "azurerm_network_security_group" "mgmt-app-gw" {
     source_port_range          = "*"
     destination_address_prefix = "10.0.4.0/29"
     destination_port_ranges    = ["65503-65534"]
+  }
+
+  security_rule {
+    name                       = "mgmtwebhooks"
+    description                = "Access for github webhooks"
+    priority                   = 2010
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "TCP"
+    source_address_prefixes      = ["${data.github_ip_ranges.whitelist.hooks}"]
+    source_port_range          = "*"
+    destination_address_prefix = "10.40.128.6/32"
+    destination_port_ranges    = ["443"]
   }
 
   security_rule {
