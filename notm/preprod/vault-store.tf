233c5826d64c0a6f39a6104a066f12ba9c238c38
Matt <34448412+mattops@users.noreply.github.com>
2019-06-05 14:02:31 +0100

    Removed azure resources- storage is created and managed by diginit script and holds the tf state file\(shouldn\'t be managed in the tf project as a resource\), everything else lives in AWS now.  We\'ve moved app insights to a seperate central instance for all apps. (#332)

--- notm/preprod/vault-store.tf
+++ notm/preprod/vault-store.tf
@@ -1,4 +1,3 @@
-
 data "aws_ssm_parameter" "api-client-secret" {
   name = "/${lower(var.tags["Service"])}/${lower(var.tags["Environment"])}/api_client_secret"
 }
@@ -9,4 +8,8 @@ data "aws_ssm_parameter" "session-cookie-secret" {
 
 data "aws_ssm_parameter" "google-tag-manager-id" {
   name = "/${lower(var.tags["Service"])}/${lower(var.tags["Environment"])}/google_tag_manager_id"
+}
+
+data "aws_ssm_parameter" "appinsights_instrumentationkey" {
+  name = "/${lower(var.tags["Environment"])}/appinsights_instrumentationkey"
 }
