604d50bf72a517b53248e6b529844733a54e8ead
Jamie Draper <draper65@googlemail.com>
2019-08-05 10:53:25 +0100

    DPS-enable-pay-leeds-PREPROD (#371)

--- prisonstaffhub/preprod/vars.tf
+++ prisonstaffhub/preprod/vars.tf
@@ -27,7 +27,7 @@ locals {
   hmpps_cookie_name              = "hmpps-session-preprod"
   google_analytics_id            = ""
   remote_auth_strategy           = "true"
-  update_attendance_prisons      = "MDI,HMI,NHI,PVI,WWI,BXI,STI"
+  update_attendance_prisons      = "MDI,HMI,NHI,PVI,WWI,BXI,STI,LEI"
   iep_change_link_enabled        = "true"
 }
 
