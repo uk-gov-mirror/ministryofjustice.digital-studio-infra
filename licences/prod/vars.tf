0aaab8ed224fbf534097d65c522843c53f39f269
bell-pa <peter.bell@digital.justice.gov.uk>
2019-08-07 17:15:34 +0100

    LIC-1061 Prod and pre-prod environments: Enable the RO_TWO_DAYS, RO_DUE and RO_OVERDUE e-mail templates. (#373)

--- licences/prod/vars.tf
+++ licences/prod/vars.tf
@@ -50,7 +50,7 @@ locals {
   remindersScheduleRo = "0 1 * * 1-5"
   scheduledJobAuto    = "yes"
   scheduledJobOverlap = "5000"
-  notifyActiveTemplates = "CA_RETURN,CA_DECISION,RO_NEW,DM_NEW"
+  notifyActiveTemplates = "CA_RETURN,CA_DECISION,RO_NEW,RO_TWO_DAYS,RO_DUE,RO_OVERDUE,DM_NEW"
   roServiceType       = "NOMIS"
   deliusApiUrl        = "https://community-api.service.hmpps.dsd.io/communityapi/api"
 }
