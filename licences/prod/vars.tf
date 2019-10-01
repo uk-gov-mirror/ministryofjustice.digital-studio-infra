277107fe36857c4be9072e64a881ca17c2151463
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2019-10-01 13:29:56 +0100

    Moving back to using the NOMIS rather than the delius API (#410)

--- licences/prod/vars.tf
+++ licences/prod/vars.tf
@@ -36,7 +36,7 @@ locals {
   scheduledJobAuto    = "yes"
   scheduledJobOverlap = "5000"
   notifyActiveTemplates = "CA_RETURN,CA_DECISION,RO_NEW,DM_NEW,DM_TO_CA_RETURN"
-  roServiceType       = "DELIUS"
+  roServiceType       = "NOMIS"
   deliusApiUrl        = "https://community-api.service.hmpps.dsd.io/communityapi/api"
   clearingOfficeEmail = "HDC.ClearingOffice@justice.gov.uk"
 }
