a9aa2440e6b1d3278ddf01240b145c181ae892d1
dteelingmoj <david.teeling@digital.justice.gov.uk>
2020-11-23 17:27:09 +0000

    DSO-567: aws-modules updated to terraform v0.12.28

--- aws-modules/monitoring/vars.tf
+++ aws-modules/monitoring/vars.tf
@@ -1,20 +1,20 @@
-variable "environment-name" { type = "string" }
+variable "environment-name" { type = string }
 
-variable "key-pair-name" { type = "string" }
+variable "key-pair-name" { type = string }
 
-variable "application-name" { type = "string" }
+variable "application-name" { type = string }
 
-variable "dns-zone-name" {type = "string"}
+variable "dns-zone-name" {type = string}
 
-variable "dns-zone-rg" {type = "string"}
+variable "dns-zone-rg" {type = string}
 
 
 # Instance and Deployment settings
 locals {
-  default_dns_zone = "${var.dns-zone-name}"
-  default_dns_resource_group = "${var.dns-zone-rg}"
-  default_application_name = "${var.application-name}"
-  default_environment_name = "${var.environment-name}"
+  default_dns_zone = var.dns-zone-name
+  default_dns_resource_group = var.dns-zone-rg
+  default_application_name = var.application-name
+  default_environment_name = var.environment-name
   default_resource_name_root = "${local.default_application_name}-${local.default_environment_name}"
   default_iam_resource_name_root = "dso-${local.default_environment_name}-cloudwatch"
   default_vpc_ip_range = "192.168.0.0/24"
@@ -24,5 +24,5 @@ locals {
   default_ec2_instance_size = "t2.small"
   default_availability_zone = "eu-west-2a"
   default_ec2_instance_private_ips = ["192.168.0.5"]
-  default_ec2_instance_key_pair = "${var.key-pair-name}"
+  default_ec2_instance_key_pair = var.key-pair-name
 }
