10745fd7a98dc194f27544e36d48ebc08641169a
dteelingmoj <david.teeling@digital.justice.gov.uk>
2020-11-24 11:04:54 +0000

    DSO-567: tf vars updated to Terraform v0.12.28

--- aws-devtest.tf
+++ aws-devtest.tf
@@ -1,33 +1,33 @@
 variable "aws_account_id" {
-  type    = "string"
+  type    = string
   default = "429061350814"
 }
 
 variable "aws_region" {
-  type    = "string"
+  type    = string
   default = "eu-west-2"
 }
 
 variable "aws_az_a" {
-  type    = "string"
+  type    = string
   default = "eu-west-2a"
 }
 
 variable "aws_az_b" {
-  type    = "string"
+  type    = string
   default = "eu-west-2b"
 }
 
 provider "aws" {
-  allowed_account_ids = ["${var.aws_account_id}"]
-  region              = "${var.aws_region}"
+  allowed_account_ids = [var.aws_account_id]
+  region              = var.aws_region
   assume_role {
     role_arn     = "arn:aws:iam::${var.aws_account_id}:role/terraform"
   }
 }
 
 variable "elastic-beanstalk-single-docker" {
-  type    = "string"
+  type    = string
   default = "64bit Amazon Linux 2017.09 v2.9.2 running Docker 17.12.0-ce"
 }
 
