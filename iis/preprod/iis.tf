2265d6fad25eba8e5944f1f542f1ec57cfdc650b
dteelingmoj <david.teeling@digital.justice.gov.uk>
2020-11-24 17:10:55 +0000

    DSO-567: iis upgraded to Terraform v0.12.28

--- iis/preprod/iis.tf
+++ iis/preprod/iis.tf
@@ -1,19 +1,19 @@
 variable "app-name" {
-    type = "string"
+    type = string
     default = "iis-preprod"
 }
 variable "tags" {
-    type = "map"
-    default {
+    type = map
+    default = {
         Service = "IIS"
         Environment = "Preprod"
     }
 }
 
 resource "azurerm_resource_group" "group" {
-    name = "${var.app-name}"
+    name = var.app-name
     location = "ukwest"
-    tags = "${var.tags}"
+    tags = var.tags
 }
 
 resource "random_id" "session-secret" {
@@ -34,53 +34,49 @@ resource "random_id" "sql-sgandalwar-password" {
 
 resource "azurerm_storage_account" "storage" {
     name = "${replace(var.app-name, "-", "")}storage"
-    resource_group_name = "${azurerm_resource_group.group.name}"
-    location = "${azurerm_resource_group.group.location}"
+    resource_group_name = azurerm_resource_group.group.name
+    location = azurerm_resource_group.group.location
     account_tier = "Standard"
     account_replication_type = "RAGRS"
-    enable_blob_encryption = true
 
-    tags = "${var.tags}"
+    tags = var.tags
 }
 
 variable "log-containers" {
-    type = "list"
+    type = list
     default = ["app-logs", "web-logs", "db-logs"]
 }
 resource "azurerm_storage_container" "logs" {
-    count = "${length(var.log-containers)}"
-    name = "${var.log-containers[count.index]}"
-    resource_group_name = "${azurerm_resource_group.group.name}"
-    storage_account_name = "${azurerm_storage_account.storage.name}"
+    count = length(var.log-containers)
+    name = var.log-containers[count.index]
+    storage_account_name = azurerm_storage_account.storage.name
     container_access_type = "private"
 }
 
 resource "azurerm_key_vault" "vault" {
-    name = "${var.app-name}"
-    resource_group_name = "${azurerm_resource_group.group.name}"
-    location = "${azurerm_resource_group.group.location}"
-    sku {
-        name = "standard"
-    }
-    tenant_id = "${var.azure_tenant_id}"
+    name = var.app-name
+    resource_group_name = azurerm_resource_group.group.name
+    location = azurerm_resource_group.group.location
+    sku_name = "standard"
+    tenant_id = var.azure_tenant_id
 
     access_policy {
-        tenant_id = "${var.azure_tenant_id}"
-        object_id = "${var.azure_webops_group_oid}"
+        tenant_id = var.azure_tenant_id
+        object_id = var.azure_webops_group_oid
         key_permissions = []
-        secret_permissions = "${var.azure_secret_permissions_all}"
+        secret_permissions = var.azure_secret_permissions_all
     }
 
     access_policy {
-        tenant_id = "${var.azure_tenant_id}"
-        object_id = "${var.azure_app_service_oid}"
+        tenant_id = var.azure_tenant_id
+        object_id = var.azure_app_service_oid
         key_permissions = []
         secret_permissions = ["get"]
     }
 
     access_policy {
-        tenant_id = "${var.azure_tenant_id}"
-        object_id = "${var.azure_jenkins_sp_oid}"
+        tenant_id = var.azure_tenant_id
+        object_id = var.azure_jenkins_sp_oid
         key_permissions = []
         secret_permissions = ["set"]
     }
@@ -89,39 +85,39 @@ resource "azurerm_key_vault" "vault" {
     enabled_for_disk_encryption = false
     enabled_for_template_deployment = true
 
-    tags = "${var.tags}"
+    tags = var.tags
 }
 
 module "sql" {
     source = "../../shared/modules/azure-sql"
-    name = "${var.app-name}"
-    resource_group = "${azurerm_resource_group.group.name}"
-    location = "${azurerm_resource_group.group.location}"
+    name = var.app-name
+    resource_group = azurerm_resource_group.group.name
+    location = azurerm_resource_group.group.location
     administrator_login = "iis"
     firewall_rules = [
         {
             label = "NOMS Studio office"
-            start = "${var.ips["office"]}"
-            end = "${var.ips["office"]}"
+            start = var.ips["office"]
+            end = var.ips["office"]
         },
         {
             label = "MOJ Digital"
-            start = "${var.ips["mojvpn"]}"
-            end = "${var.ips["mojvpn"]}"
+            start = var.ips["mojvpn"]
+            end = var.ips["mojvpn"]
         },
     ]
-    audit_storage_account = "${azurerm_storage_account.storage.name}"
+    audit_storage_account = azurerm_storage_account.storage.name
     edition = "Standard"
     scale = "S3"
     space_gb = 250
     collation = "Latin1_General_CS_AS"
-    tags = "${var.tags}"
+    tags = var.tags
 
-    db_users {
-        iisuser = "${random_id.sql-iisuser-password.b64}"
-        atodd = "${random_id.sql-atodd-password.b64}"
-        mwhitfield = "${random_id.sql-mwhitfield-password.b64}"
-        sgandalwar = "${random_id.sql-sgandalwar-password.b64}"
+    db_users = {
+        iisuser = random_id.sql-iisuser-password.b64_std
+        atodd = random_id.sql-atodd-password.b64_std
+        mwhitfield = random_id.sql-mwhitfield-password.b64_std
+        sgandalwar = random_id.sql-sgandalwar-password.b64_std
     }
 
     setup_queries = [
@@ -145,23 +141,23 @@ module "sql" {
 }
 
 resource "azurerm_sql_firewall_rule" "app-access" {
-    count = "${length(split(",", azurerm_template_deployment.webapp.outputs["ips"]))}"
+    count = length(split(",", azurerm_template_deployment.webapp.outputs["ips"]))
     name = "Application IP ${count.index}"
-    resource_group_name = "${azurerm_resource_group.group.name}"
-    server_name = "${module.sql.server_name}"
-    start_ip_address = "${element(split(",", azurerm_template_deployment.webapp.outputs["ips"]), count.index)}"
-    end_ip_address = "${element(split(",", azurerm_template_deployment.webapp.outputs["ips"]), count.index)}"
+    resource_group_name = azurerm_resource_group.group.name
+    server_name = module.sql.server_name
+    start_ip_address = element(split(",", azurerm_template_deployment.webapp.outputs["ips"]), count.index)
+    end_ip_address = element(split(",", azurerm_template_deployment.webapp.outputs["ips"]), count.index)
 }
 
 resource "azurerm_template_deployment" "webapp" {
     name = "webapp"
-    resource_group_name = "${azurerm_resource_group.group.name}"
+    resource_group_name = azurerm_resource_group.group.name
     deployment_mode = "Incremental"
-    template_body = "${file("../../shared/appservice.template.json")}"
-    parameters {
-        name = "${var.app-name}"
-        service = "${var.tags["Service"]}"
-        environment = "${var.tags["Environment"]}"
+    template_body = file("../../shared/appservice.template.json")
+    parameters = {
+        name = var.app-name
+        service = var.tags["Service"]
+        environment = var.tags["Environment"]
         workers = "2"
         sku_name = "S2"
         sku_tier = "Standard"
@@ -170,11 +166,11 @@ resource "azurerm_template_deployment" "webapp" {
 
 data "external" "sas-url" {
     program = ["python3", "../../tools/container-sas-url-cli-auth.py"]
-    query {
-        subscription_id = "${var.azure_subscription_id}"
-        tenant_id = "${var.azure_tenant_id}"
-        resource_group = "${azurerm_resource_group.group.name}"
-        storage_account = "${azurerm_storage_account.storage.name}"
+    query = {
+        subscription_id = var.azure_subscription_id
+        tenant_id = var.azure_tenant_id
+        resource_group = azurerm_resource_group.group.name
+        storage_account = azurerm_storage_account.storage.name
         container = "web-logs"
         permissions = "rwdl"
         start_date = "2017-05-15T00:00:00Z"
@@ -184,53 +180,53 @@ data "external" "sas-url" {
 
 resource "azurerm_template_deployment" "webapp-weblogs" {
     name = "webapp-weblogs"
-    resource_group_name = "${azurerm_resource_group.group.name}"
+    resource_group_name = azurerm_resource_group.group.name
     deployment_mode = "Incremental"
-    template_body = "${file("../../shared/appservice-weblogs.template.json")}"
+    template_body = file("../../shared/appservice-weblogs.template.json")
 
-    parameters {
-        name = "${azurerm_template_deployment.webapp.parameters.name}"
-        storageSAS = "${data.external.sas-url.result["url"]}"
+    parameters = {
+        name = azurerm_template_deployment.webapp.parameters.name
+        storageSAS = data.external.sas-url.result["url"]
     }
 
-    depends_on = ["azurerm_template_deployment.webapp"]
+    depends_on = [azurerm_template_deployment.webapp]
 }
 
 resource "azurerm_template_deployment" "insights" {
-    name = "${var.app-name}"
-    resource_group_name = "${azurerm_resource_group.group.name}"
+    name = var.app-name
+    resource_group_name = azurerm_resource_group.group.name
     deployment_mode = "Incremental"
-    template_body = "${file("../../shared/insights.template.json")}"
-    parameters {
-        name = "${azurerm_template_deployment.webapp.parameters.name}"
+    template_body = file("../../shared/insights.template.json")
+    parameters = {
+        name = azurerm_template_deployment.webapp.parameters.name
         location = "northeurope" // Not in UK yet
-        service = "${var.tags["Service"]}"
-        environment = "${var.tags["Environment"]}"
-        appServiceId = "${azurerm_template_deployment.webapp.outputs["resourceId"]}"
+        service = var.tags["Service"]
+        environment = var.tags["Environment"]
+        appServiceId = azurerm_template_deployment.webapp.outputs["resourceId"]
     }
 }
 
 resource "azurerm_template_deployment" "webapp-whitelist" {
     name = "webapp-whitelist"
-    resource_group_name = "${azurerm_resource_group.group.name}"
+    resource_group_name = azurerm_resource_group.group.name
     deployment_mode = "Incremental"
-    template_body = "${file("../../shared/appservice-whitelist.template.json")}"
+    template_body = file("../../shared/appservice-whitelist.template.json")
 
-    parameters {
-        name = "${azurerm_template_deployment.webapp.parameters.name}"
-        ip1 = "${var.ips["office"]}"
-        ip2 = "${var.ips["quantum"]}"
-        ip3 = "${var.ips["quantum_alt"]}",
-        ip4 = "${var.ips["health-kick"]}"
+    parameters = {
+        name = azurerm_template_deployment.webapp.parameters.name
+        ip1 = var.ips["office"]
+        ip2 = var.ips["quantum"]
+        ip3 = var.ips["quantum_alt"]
+        ip4 = var.ips["health-kick"]
     }
 
-    depends_on = ["azurerm_template_deployment.webapp"]
+    depends_on = [azurerm_template_deployment.webapp]
 }
 
 data "external" "vault" {
     program = ["python3", "../../tools/keyvault-data-cli-auth.py"]
-    query {
-        vault = "${azurerm_key_vault.vault.name}"
+    query = {
+        vault = azurerm_key_vault.vault.name
 
         client_id = "signon-client-id"
         client_secret = "signon-client-secret"
@@ -241,58 +237,58 @@ data "external" "vault" {
 
 resource "azurerm_template_deployment" "webapp-config" {
     name = "webapp-config"
-    resource_group_name = "${azurerm_resource_group.group.name}"
+    resource_group_name = azurerm_resource_group.group.name
     deployment_mode = "Incremental"
-    template_body = "${file("../webapp-config.template.json")}"
+    template_body = file("../webapp-config.template.json")
 
-    parameters {
-        name = "${azurerm_template_deployment.webapp.parameters.name}"
+    parameters = {
+        name = azurerm_template_deployment.webapp.parameters.name
         DB_USER = "iisuser"
-        DB_PASS = "${random_id.sql-iisuser-password.b64}"
-        DB_SERVER = "${module.sql.db_server}"
-        DB_NAME = "${module.sql.db_name}"
-        SESSION_SECRET = "${random_id.session-secret.b64}"
-        CLIENT_ID = "${data.external.vault.result["client_id"]}"
-        CLIENT_SECRET = "${data.external.vault.result["client_secret"]}"
+        DB_PASS = random_id.sql-iisuser-password.b64_std
+        DB_SERVER = module.sql.db_server
+        DB_NAME = module.sql.db_name
+        SESSION_SECRET = random_id.session-secret.b64_std
+        CLIENT_ID = data.external.vault.result["client_id"]
+        CLIENT_SECRET = data.external.vault.result["client_secret"]
         TOKEN_HOST = "https://signon.service.justice.gov.uk"
-        ADMINISTRATORS = "${data.external.vault.result["administrators"]}"
-        APPINSIGHTS_INSTRUMENTATIONKEY = "${azurerm_template_deployment.insights.outputs["instrumentationKey"]}"
+        ADMINISTRATORS = data.external.vault.result["administrators"]
+        APPINSIGHTS_INSTRUMENTATIONKEY = azurerm_template_deployment.insights.outputs["instrumentationKey"]
     }
 
-    depends_on = ["azurerm_template_deployment.webapp"]
+    depends_on = [azurerm_template_deployment.webapp]
 }
 
 resource "azurerm_template_deployment" "webapp-ssl" {
     name = "webapp-ssl"
-    resource_group_name = "${azurerm_resource_group.group.name}"
+    resource_group_name = azurerm_resource_group.group.name
     deployment_mode = "Incremental"
-    template_body = "${file("../../shared/appservice-ssl.template.json")}"
+    template_body = file("../../shared/appservice-ssl.template.json")
 
-    parameters {
-        name = "${azurerm_template_deployment.webapp.parameters.name}"
+    parameters = {
+        name = azurerm_template_deployment.webapp.parameters.name
         hostname = "${azurerm_dns_cname_record.cname.name}.${azurerm_dns_cname_record.cname.zone_name}"
-        keyVaultId = "${azurerm_key_vault.vault.id}"
+        keyVaultId = azurerm_key_vault.vault.id
         keyVaultCertName = "hpa-preprodDOTserviceDOThmppsDOTdsdDOTio"
-        service = "${var.tags["Service"]}"
-        environment = "${var.tags["Environment"]}"
+        service = var.tags["Service"]
+        environment = var.tags["Environment"]
     }
 
-    depends_on = ["azurerm_template_deployment.webapp"]
+    depends_on = [azurerm_template_deployment.webapp]
 }
 
 resource "azurerm_template_deployment" "webapp-github" {
     name = "webapp-github"
-    resource_group_name = "${azurerm_resource_group.group.name}"
+    resource_group_name = azurerm_resource_group.group.name
     deployment_mode = "Incremental"
-    template_body = "${file("../../shared/appservice-scm.template.json")}"
+    template_body = file("../../shared/appservice-scm.template.json")
 
-    parameters {
-        name = "${var.app-name}"
+    parameters = {
+        name = var.app-name
         repoURL = "https://github.com/ministryofjustice/iis.git"
         branch = "deploy-to-preprod"
     }
 
-    depends_on = ["azurerm_template_deployment.webapp"]
+    depends_on = [azurerm_template_deployment.webapp]
 }
 
 
@@ -311,7 +307,7 @@ resource "github_repository_webhook" "webapp-deploy" {
 
 module "slackhook" {
     source = "../../shared/modules/slackhook"
-    app_name = "${azurerm_template_deployment.webapp.parameters.name}"
+    app_name = azurerm_template_deployment.webapp.parameters.name
     azure_subscription = "production"
     channels = ["hpa"]
 }
@@ -322,7 +318,7 @@ resource "azurerm_dns_cname_record" "cname" {
     resource_group_name = "webops-prod"
     ttl = "300"
     record = "${var.app-name}.azurewebsites.net"
-    tags = "${var.tags}"
+    tags = var.tags
 }
 
 output "advice" {
--- iis/stage/iis.tf
+++ iis/stage/iis.tf
@@ -1,19 +1,19 @@
 variable "app-name" {
-    type = "string"
+    type = string
     default = "iis-stage"
 }
 variable "tags" {
-    type = "map"
-    default {
+    type = map
+    default = {
         Service = "IIS"
         Environment = "Stage"
     }
 }
 
 resource "azurerm_resource_group" "group" {
-    name = "${var.app-name}"
+    name = var.app-name
     location = "ukwest"
-    tags = "${var.tags}"
+    tags = var.tags
 }
 
 resource "random_id" "session-secret" {
@@ -25,60 +25,56 @@ resource "random_id" "sql-user-password" {
 
 resource "azurerm_storage_account" "storage" {
     name = "${replace(var.app-name, "-", "")}storage"
-    resource_group_name = "${azurerm_resource_group.group.name}"
-    location = "${azurerm_resource_group.group.location}"
+    resource_group_name = azurerm_resource_group.group.name
+    location = azurerm_resource_group.group.location
     account_tier = "Standard"
     account_replication_type = "RAGRS"
-    enable_blob_encryption = true
 
-    tags = "${var.tags}"
+    tags = var.tags
 }
 
 variable "log-containers" {
-    type = "list"
+    type = list
     default = ["app-logs", "web-logs", "db-logs"]
 }
 resource "azurerm_storage_container" "logs" {
-    count = "${length(var.log-containers)}"
-    name = "${var.log-containers[count.index]}"
-    resource_group_name = "${azurerm_resource_group.group.name}"
-    storage_account_name = "${azurerm_storage_account.storage.name}"
+    count = length(var.log-containers)
+    name = var.log-containers[count.index]
+    storage_account_name = azurerm_storage_account.storage.name
     container_access_type = "private"
 }
 
 resource "azurerm_key_vault" "vault" {
-    name = "${var.app-name}"
-    resource_group_name = "${azurerm_resource_group.group.name}"
-    location = "${azurerm_resource_group.group.location}"
-    sku {
-        name = "standard"
-    }
-    tenant_id = "${var.azure_tenant_id}"
+    name = var.app-name
+    resource_group_name = azurerm_resource_group.group.name
+    location = azurerm_resource_group.group.location
+    sku_name = "standard"
+    tenant_id = var.azure_tenant_id
 
     access_policy {
-        tenant_id = "${var.azure_tenant_id}"
-        object_id = "${var.azure_webops_group_oid}"
+        tenant_id = var.azure_tenant_id
+        object_id = var.azure_webops_group_oid
         key_permissions = []
-        secret_permissions = "${var.azure_secret_permissions_all}"
+        secret_permissions = var.azure_secret_permissions_all
     }
 
     access_policy {
-        tenant_id = "${var.azure_tenant_id}"
-        object_id = "${var.azure_iis_group_oid}"
+        tenant_id = var.azure_tenant_id
+        object_id = var.azure_iis_group_oid
         key_permissions = []
-        secret_permissions = "${var.azure_secret_permissions_all}"
+        secret_permissions = var.azure_secret_permissions_all
     }
 
     access_policy {
-        tenant_id = "${var.azure_tenant_id}"
-        object_id = "${var.azure_jenkins_sp_oid}"
+        tenant_id = var.azure_tenant_id
+        object_id = var.azure_jenkins_sp_oid
         key_permissions = []
         secret_permissions = ["set"]
     }
 
     access_policy {
-        tenant_id = "${var.azure_tenant_id}"
-        object_id = "${var.azure_app_service_oid}"
+        tenant_id = var.azure_tenant_id
+        object_id = var.azure_app_service_oid
         key_permissions = []
         secret_permissions = ["get"]
     }
@@ -87,14 +83,14 @@ resource "azurerm_key_vault" "vault" {
     enabled_for_disk_encryption = false
     enabled_for_template_deployment = true
 
-    tags = "${var.tags}"
+    tags = var.tags
 }
 
 module "sql" {
     source = "../../shared/modules/azure-sql"
-    name = "${var.app-name}"
-    resource_group = "${azurerm_resource_group.group.name}"
-    location = "${azurerm_resource_group.group.location}"
+    name = var.app-name
+    resource_group = azurerm_resource_group.group.name
+    location = azurerm_resource_group.group.location
     administrator_login = "iis"
     firewall_rules = [
         {
@@ -103,21 +99,21 @@ module "sql" {
             end = "255.255.255.255"
         },
     ]
-    audit_storage_account = "${azurerm_storage_account.storage.name}"
+    audit_storage_account = azurerm_storage_account.storage.name
     edition = "Basic"
     collation = "Latin1_General_CS_AS"
-    tags = "${var.tags}"
+    tags = var.tags
 }
 
 resource "azurerm_template_deployment" "webapp" {
     name = "webapp"
-    resource_group_name = "${azurerm_resource_group.group.name}"
+    resource_group_name = azurerm_resource_group.group.name
     deployment_mode = "Incremental"
-    template_body = "${file("../../shared/appservice.template.json")}"
-    parameters {
-        name = "${var.app-name}"
-        service = "${var.tags["Service"]}"
-        environment = "${var.tags["Environment"]}"
+    template_body = file("../../shared/appservice.template.json")
+    parameters = {
+        name = var.app-name
+        service = var.tags["Service"]
+        environment = var.tags["Environment"]
         workers = "2"
         sku_name = "S1"
         sku_tier = "Standard"
@@ -126,11 +122,11 @@ resource "azurerm_template_deployment" "webapp" {
 
 data "external" "sas-url" {
     program = ["python3", "../../tools/container-sas-url-cli-auth.py"]
-    query {
-        subscription_id = "${var.azure_subscription_id}"
-        tenant_id = "${var.azure_tenant_id}"
-        resource_group = "${azurerm_resource_group.group.name}"
-        storage_account = "${azurerm_storage_account.storage.name}"
+    query = {
+        subscription_id = var.azure_subscription_id
+        tenant_id = var.azure_tenant_id
+        resource_group = azurerm_resource_group.group.name
+        storage_account = azurerm_storage_account.storage.name
         container = "web-logs"
         permissions = "rwdl"
         start_date = "2017-05-15T00:00:00Z"
@@ -140,51 +136,51 @@ data "external" "sas-url" {
 
 resource "azurerm_template_deployment" "webapp-weblogs" {
     name = "webapp-weblogs"
-    resource_group_name = "${azurerm_resource_group.group.name}"
+    resource_group_name = azurerm_resource_group.group.name
     deployment_mode = "Incremental"
-    template_body = "${file("../../shared/appservice-weblogs.template.json")}"
+    template_body = file("../../shared/appservice-weblogs.template.json")
 
-    parameters {
-        name = "${azurerm_template_deployment.webapp.parameters.name}"
-        storageSAS = "${data.external.sas-url.result.url}"
+    parameters = {
+        name = azurerm_template_deployment.webapp.parameters.name
+        storageSAS = data.external.sas-url.result.url
     }
 
-    depends_on = ["azurerm_template_deployment.webapp"]
+    depends_on = [azurerm_template_deployment.webapp]
 }
 
 resource "azurerm_template_deployment" "insights" {
-    name = "${var.app-name}"
-    resource_group_name = "${azurerm_resource_group.group.name}"
+    name = var.app-name
+    resource_group_name = azurerm_resource_group.group.name
     deployment_mode = "Incremental"
-    template_body = "${file("../../shared/insights.template.json")}"
-    parameters {
-        name = "${azurerm_template_deployment.webapp.parameters.name}"
+    template_body = file("../../shared/insights.template.json")
+    parameters = {
+        name = azurerm_template_deployment.webapp.parameters.name
         location = "northeurope" // Not in UK yet
-        service = "${var.tags["Service"]}"
-        environment = "${var.tags["Environment"]}"
-        appServiceId = "${azurerm_template_deployment.webapp.outputs["resourceId"]}"
+        service = var.tags["Service"]
+        environment = var.tags["Environment"]
+        appServiceId = azurerm_template_deployment.webapp.outputs["resourceId"]
     }
 }
 
 resource "azurerm_template_deployment" "webapp-whitelist" {
     name = "webapp-whitelist"
-    resource_group_name = "${azurerm_resource_group.group.name}"
+    resource_group_name = azurerm_resource_group.group.name
     deployment_mode = "Incremental"
-    template_body = "${file("../../shared/appservice-whitelist.template.json")}"
+    template_body = file("../../shared/appservice-whitelist.template.json")
 
-    parameters {
-        name = "${azurerm_template_deployment.webapp.parameters.name}"
+    parameters = {
+        name = azurerm_template_deployment.webapp.parameters.name
         ip1 = "0.0.0.0"
         subnet1 = "0.0.0.0"
     }
 
-    depends_on = ["azurerm_template_deployment.webapp"]
+    depends_on = [azurerm_template_deployment.webapp]
 }
 
 data "external" "vault" {
     program = ["python3", "../../tools/keyvault-data-cli-auth.py"]
-    query {
-        vault = "${azurerm_key_vault.vault.name}"
+    query = {
+        vault = azurerm_key_vault.vault.name
 
         client_id = "signon-client-id"
         client_secret = "signon-client-secret"
@@ -195,48 +191,48 @@ data "external" "vault" {
 
 resource "azurerm_template_deployment" "webapp-config" {
     name = "webapp-config"
-    resource_group_name = "${azurerm_resource_group.group.name}"
+    resource_group_name = azurerm_resource_group.group.name
     deployment_mode = "Incremental"
-    template_body = "${file("../webapp-config.template.json")}"
+    template_body = file("../webapp-config.template.json")
 
-    parameters {
-        name = "${azurerm_template_deployment.webapp.parameters.name}"
+    parameters = {
+        name = azurerm_template_deployment.webapp.parameters.name
         DB_USER = "iisuser"
-        DB_PASS = "${random_id.sql-user-password.b64}"
-        DB_SERVER = "${module.sql.db_server}"
-        DB_NAME = "${module.sql.db_name}"
-        SESSION_SECRET = "${random_id.session-secret.b64}"
-        CLIENT_ID = "${data.external.vault.result.client_id}"
-        CLIENT_SECRET = "${data.external.vault.result.client_secret}"
+        DB_PASS = random_id.sql-user-password.b64_std
+        DB_SERVER = module.sql.db_server
+        DB_NAME = module.sql.db_name
+        SESSION_SECRET = random_id.session-secret.b64_std
+        CLIENT_ID = data.external.vault.result.client_id
+        CLIENT_SECRET = data.external.vault.result.client_secret
         TOKEN_HOST = "https://www.signon.dsd.io"
-        ADMINISTRATORS = "${data.external.vault.result.administrators}"
-        APPINSIGHTS_INSTRUMENTATIONKEY = "${azurerm_template_deployment.insights.outputs["instrumentationKey"]}"
+        ADMINISTRATORS = data.external.vault.result.administrators
+        APPINSIGHTS_INSTRUMENTATIONKEY = azurerm_template_deployment.insights.outputs["instrumentationKey"]
     }
 
-    depends_on = ["azurerm_template_deployment.webapp"]
+    depends_on = [azurerm_template_deployment.webapp]
 }
 
 resource "azurerm_template_deployment" "webapp-ssl" {
     name = "webapp-ssl"
-    resource_group_name = "${azurerm_resource_group.group.name}"
+    resource_group_name = azurerm_resource_group.group.name
     deployment_mode = "Incremental"
-    template_body = "${file("../../shared/appservice-ssl.template.json")}"
+    template_body = file("../../shared/appservice-ssl.template.json")
 
-    parameters {
-        name = "${azurerm_template_deployment.webapp.parameters.name}"
+    parameters = {
+        name = azurerm_template_deployment.webapp.parameters.name
         hostname = "${azurerm_dns_cname_record.cname.name}.${azurerm_dns_cname_record.cname.zone_name}"
-        keyVaultId = "${azurerm_key_vault.vault.id}"
+        keyVaultId = azurerm_key_vault.vault.id
         keyVaultCertName = "hpa-stageDOThmppsDOTdsdDOTio"
-        service = "${var.tags["Service"]}"
-        environment = "${var.tags["Environment"]}"
+        service = var.tags["Service"]
+        environment = var.tags["Environment"]
     }
 
-    depends_on = ["azurerm_template_deployment.webapp"]
+    depends_on = [azurerm_template_deployment.webapp]
 }
 
 module "slackhook" {
     source = "../../shared/modules/slackhook"
-    app_name = "${azurerm_template_deployment.webapp.parameters.name}"
+    app_name = azurerm_template_deployment.webapp.parameters.name
     channels = ["hpa"]
 }
 
@@ -246,7 +242,7 @@ resource "azurerm_dns_cname_record" "cname" {
     resource_group_name = "webops"
     ttl = "300"
     record = "${var.app-name}.azurewebsites.net"
-    tags = "${var.tags}"
+    tags = var.tags
 }
 
 output "advice" {
