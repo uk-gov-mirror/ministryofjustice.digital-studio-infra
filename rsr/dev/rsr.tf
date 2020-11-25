e8283630681c797ddf67414eca01032458a7572c
dteelingmoj <david.teeling@digital.justice.gov.uk>
2020-11-25 12:06:45 +0000

    DSO-567: rsr upgraded to Terraform v0.12.28

--- rsr/dev/rsr.tf
+++ rsr/dev/rsr.tf
@@ -1,27 +1,27 @@
 variable "app-name" {
-  type    = "string"
+  type    = string
   default = "rsr-dev"
 }
 
 variable "tags" {
-  type = "map"
+  type = map
 
-  default {
+  default = {
     Service     = "RSR"
     Environment = "Dev"
   }
 }
 
 resource "azurerm_resource_group" "group" {
-  name     = "${var.app-name}"
+  name     = var.app-name
   location = "ukwest"
-  tags     = "${var.tags}"
+  tags     = var.tags
 }
 
 resource "azurerm_app_service_plan" "app" {
-  name                = "${var.app-name}"
-  location            = "${azurerm_resource_group.group.location}"
-  resource_group_name = "${azurerm_resource_group.group.name}"
+  name                = var.app-name
+  location            = azurerm_resource_group.group.location
+  resource_group_name = azurerm_resource_group.group.name
 
   sku {
     tier     = "Standard"
@@ -29,76 +29,73 @@ resource "azurerm_app_service_plan" "app" {
     capacity = 1
   }
 
-  tags = "${var.tags}"
+  tags = var.tags
 }
 
 resource "azurerm_app_service" "app" {
-  name                = "${var.app-name}"
-  location            = "${azurerm_resource_group.group.location}"
-  resource_group_name = "${azurerm_resource_group.group.name}"
-  app_service_plan_id = "${azurerm_app_service_plan.app.id}"
+  name                = var.app-name
+  location            = azurerm_resource_group.group.location
+  resource_group_name = azurerm_resource_group.group.name
+  app_service_plan_id = azurerm_app_service_plan.app.id
 
-  tags = "${var.tags}"
+  tags = var.tags
 
-  app_settings {
+  app_settings = {
     WEBSITE_NODE_DEFAULT_VERSION   = "6.9.1"
-    APPINSIGHTS_INSTRUMENTATIONKEY = "${azurerm_application_insights.insights.instrumentation_key}"
+    APPINSIGHTS_INSTRUMENTATIONKEY = azurerm_application_insights.insights.instrumentation_key
   }
 }
 
 resource "azurerm_application_insights" "insights" {
-  name                = "${var.app-name}"
+  name                = var.app-name
   location            = "North Europe"
-  resource_group_name = "${azurerm_resource_group.group.name}"
-  application_type    = "Web"
+  resource_group_name = azurerm_resource_group.group.name
+  application_type    = "web"
 }
 
 resource "azurerm_storage_account" "storage" {
   name                     = "${replace(var.app-name, "-", "")}storage"
-  resource_group_name      = "${azurerm_resource_group.group.name}"
-  location                 = "${azurerm_resource_group.group.location}"
+  resource_group_name      = azurerm_resource_group.group.name
+  location                 = azurerm_resource_group.group.location
   account_tier             = "Standard"
   account_replication_type = "RAGRS"
-  enable_blob_encryption   = true
 
-  tags = "${var.tags}"
+  tags = var.tags
 }
 
 resource "azurerm_key_vault" "vault" {
-  name                = "${var.app-name}"
-  resource_group_name = "${azurerm_resource_group.group.name}"
-  location            = "${azurerm_resource_group.group.location}"
+  name                = var.app-name
+  resource_group_name = azurerm_resource_group.group.name
+  location            = azurerm_resource_group.group.location
 
-  sku {
-    name = "standard"
-  }
+  sku_name = "standard"
 
-  tenant_id = "${var.azure_tenant_id}"
+  tenant_id = var.azure_tenant_id
 
   access_policy {
-    tenant_id          = "${var.azure_tenant_id}"
-    object_id          = "${var.azure_webops_group_oid}"
+    tenant_id          = var.azure_tenant_id
+    object_id          = var.azure_webops_group_oid
     key_permissions    = []
-    secret_permissions = "${var.azure_secret_permissions_all}"
+    secret_permissions = var.azure_secret_permissions_all
   }
 
   access_policy {
-    tenant_id          = "${var.azure_tenant_id}"
-    object_id          = "${var.azure_aap_group_oid}"
+    tenant_id          = var.azure_tenant_id
+    object_id          = var.azure_aap_group_oid
     key_permissions    = []
-    secret_permissions = "${var.azure_secret_permissions_all}"
+    secret_permissions = var.azure_secret_permissions_all
   }
 
   access_policy {
-    tenant_id          = "${var.azure_tenant_id}"
-    object_id          = "${var.azure_jenkins_sp_oid}"
+    tenant_id          = var.azure_tenant_id
+    object_id          = var.azure_jenkins_sp_oid
     key_permissions    = []
     secret_permissions = ["set"]
   }
 
   access_policy {
-    tenant_id          = "${var.azure_tenant_id}"
-    object_id          = "${var.azure_app_service_oid}"
+    tenant_id          = var.azure_tenant_id
+    object_id          = var.azure_app_service_oid
     key_permissions    = []
     secret_permissions = ["get"]
   }
@@ -107,49 +104,49 @@ resource "azurerm_key_vault" "vault" {
   enabled_for_disk_encryption     = false
   enabled_for_template_deployment = true
 
-  tags = "${var.tags}"
+  tags = var.tags
 }
 
 resource "azurerm_dns_cname_record" "rsr" {
-  name                = "${var.app-name}"
+  name                = var.app-name
   zone_name           = "hmpps.dsd.io"
   resource_group_name = "webops"
   ttl                 = "300"
   record              = "${var.app-name}.azurewebsites.net"
-  tags                = "${var.tags}"
+  tags                = var.tags
 }
 
 resource "azurerm_template_deployment" "rsr-ssl" {
   name                = "rsr-ssl"
-  resource_group_name = "${azurerm_resource_group.group.name}"
+  resource_group_name = azurerm_resource_group.group.name
   deployment_mode     = "Incremental"
-  template_body       = "${file("../../shared/appservice-ssl.template.json")}"
+  template_body       = file("../../shared/appservice-ssl.template.json")
 
-  parameters {
-    name             = "${azurerm_app_service.app.name}"
+  parameters = {
+    name             = azurerm_app_service.app.name
     hostname         = "${azurerm_dns_cname_record.rsr.name}.${azurerm_dns_cname_record.rsr.zone_name}"
-    keyVaultId       = "${azurerm_key_vault.vault.id}"
-    keyVaultCertName = "${replace("${azurerm_dns_cname_record.rsr.name}.${azurerm_dns_cname_record.rsr.zone_name}", ".", "DOT")}"
-    service          = "${var.tags["Service"]}"
-    environment      = "${var.tags["Environment"]}"
+    keyVaultId       = azurerm_key_vault.vault.id
+    keyVaultCertName = replace("${azurerm_dns_cname_record.rsr.name}.${azurerm_dns_cname_record.rsr.zone_name}", ".", "DOT")
+    service          = var.tags["Service"]
+    environment      = var.tags["Environment"]
   }
 
-  depends_on = ["azurerm_app_service.app"]
+  depends_on = [azurerm_app_service.app]
 }
 
 resource "azurerm_template_deployment" "rsr-github" {
   name                = "rsr-github"
-  resource_group_name = "${azurerm_resource_group.group.name}"
+  resource_group_name = azurerm_resource_group.group.name
   deployment_mode     = "Incremental"
-  template_body       = "${file("../../shared/appservice-scm.template.json")}"
+  template_body       = file("../../shared/appservice-scm.template.json")
 
-  parameters {
-    name    = "${azurerm_app_service.app.name}"
+  parameters = {
+    name    = azurerm_app_service.app.name
     repoURL = "https://github.com/ministryofjustice/rsr-calculator-service.git"
     branch  = "deploy-to-dev"
   }
 
-  depends_on = ["azurerm_app_service.app"]
+  depends_on = [azurerm_app_service.app]
 }
 
 resource "github_repository_webhook" "rsr-deploy" {
@@ -167,6 +164,6 @@ resource "github_repository_webhook" "rsr-deploy" {
 
 module "slackhook-rsr" {
   source   = "../../shared/modules/slackhook"
-  app_name = "${azurerm_app_service.app.name}"
+  app_name = azurerm_app_service.app.name
   channels = ["api-accelerator"]
 }
--- rsr/prod/rsr.tf
+++ rsr/prod/rsr.tf
@@ -1,62 +1,59 @@
 variable "app-name" {
-  type    = "string"
+  type    = string
   default = "rsr-prod"
 }
 
 variable "tags" {
-  type = "map"
+  type = map
 
-  default {
+  default = {
     Service     = "RSR"
     Environment = "Prod"
   }
 }
 
 resource "azurerm_resource_group" "group" {
-  name     = "${var.app-name}"
+  name     = var.app-name
   location = "ukwest"
-  tags     = "${var.tags}"
+  tags     = var.tags
 }
 
 resource "azurerm_storage_account" "storage" {
   name                     = "${replace(var.app-name, "-", "")}storage"
-  resource_group_name      = "${azurerm_resource_group.group.name}"
-  location                 = "${azurerm_resource_group.group.location}"
+  resource_group_name      = azurerm_resource_group.group.name
+  location                 = azurerm_resource_group.group.location
   account_tier             = "Standard"
   account_replication_type = "RAGRS"
-  enable_blob_encryption   = true
 
-  tags = "${var.tags}"
+  tags = var.tags
 }
 
 resource "azurerm_key_vault" "vault" {
-  name                = "${var.app-name}"
-  resource_group_name = "${azurerm_resource_group.group.name}"
-  location            = "${azurerm_resource_group.group.location}"
+  name                = var.app-name
+  resource_group_name = azurerm_resource_group.group.name
+  location            = azurerm_resource_group.group.location
 
-  sku {
-    name = "standard"
-  }
+  sku_name = "standard"
 
-  tenant_id = "${var.azure_tenant_id}"
+  tenant_id = var.azure_tenant_id
 
   access_policy {
-    tenant_id          = "${var.azure_tenant_id}"
-    object_id          = "${var.azure_webops_group_oid}"
+    tenant_id          = var.azure_tenant_id
+    object_id          = var.azure_webops_group_oid
     key_permissions    = []
-    secret_permissions = "${var.azure_secret_permissions_all}"
+    secret_permissions = var.azure_secret_permissions_all
   }
 
   access_policy {
-    tenant_id          = "${var.azure_tenant_id}"
-    object_id          = "${var.azure_app_service_oid}"
+    tenant_id          = var.azure_tenant_id
+    object_id          = var.azure_app_service_oid
     key_permissions    = []
     secret_permissions = ["get"]
   }
 
   access_policy {
-    tenant_id          = "${var.azure_tenant_id}"
-    object_id          = "${var.azure_jenkins_sp_oid}"
+    tenant_id          = var.azure_tenant_id
+    object_id          = var.azure_jenkins_sp_oid
     key_permissions    = []
     secret_permissions = ["set"]
   }
@@ -65,13 +62,13 @@ resource "azurerm_key_vault" "vault" {
   enabled_for_disk_encryption     = false
   enabled_for_template_deployment = true
 
-  tags = "${var.tags}"
+  tags = var.tags
 }
 
 resource "azurerm_app_service_plan" "app" {
-  name                = "${var.app-name}"
-  location            = "${azurerm_resource_group.group.location}"
-  resource_group_name = "${azurerm_resource_group.group.name}"
+  name                = var.app-name
+  location            = azurerm_resource_group.group.location
+  resource_group_name = azurerm_resource_group.group.name
 
   sku {
     tier     = "Standard"
@@ -79,28 +76,28 @@ resource "azurerm_app_service_plan" "app" {
     capacity = 1
   }
 
-  tags = "${var.tags}"
+  tags = var.tags
 }
 
 resource "azurerm_app_service" "app" {
-  name                = "${var.app-name}"
-  location            = "${azurerm_resource_group.group.location}"
-  resource_group_name = "${azurerm_resource_group.group.name}"
-  app_service_plan_id = "${azurerm_app_service_plan.app.id}"
+  name                = var.app-name
+  location            = azurerm_resource_group.group.location
+  resource_group_name = azurerm_resource_group.group.name
+  app_service_plan_id = azurerm_app_service_plan.app.id
 
-  tags = "${var.tags}"
+  tags = var.tags
 
-  app_settings {
+  app_settings = {
     WEBSITE_NODE_DEFAULT_VERSION   = "6.9.1"
-    APPINSIGHTS_INSTRUMENTATIONKEY = "${azurerm_application_insights.insights.instrumentation_key}"
+    APPINSIGHTS_INSTRUMENTATIONKEY = azurerm_application_insights.insights.instrumentation_key
   }
 }
 
 resource "azurerm_application_insights" "insights" {
-  name                = "${var.app-name}"
+  name                = var.app-name
   location            = "North Europe"
-  resource_group_name = "${azurerm_resource_group.group.name}"
-  application_type    = "Web"
+  resource_group_name = azurerm_resource_group.group.name
+  application_type    = "web"
 }
 
 resource "azurerm_dns_cname_record" "rsr" {
@@ -109,30 +106,30 @@ resource "azurerm_dns_cname_record" "rsr" {
   resource_group_name = "webops-prod"
   ttl                 = "300"
   record              = "${var.app-name}.azurewebsites.net"
-  tags                = "${var.tags}"
+  tags                = var.tags
 }
 
 resource "azurerm_template_deployment" "rsr-ssl" {
   name                = "rsr-ssl"
-  resource_group_name = "${azurerm_resource_group.group.name}"
+  resource_group_name = azurerm_resource_group.group.name
   deployment_mode     = "Incremental"
-  template_body       = "${file("../../shared/appservice-ssl.template.json")}"
+  template_body       = file("../../shared/appservice-ssl.template.json")
 
-  parameters {
-    name             = "${azurerm_app_service.app.name}"
+  parameters = {
+    name             = azurerm_app_service.app.name
     hostname         = "${azurerm_dns_cname_record.rsr.name}.${azurerm_dns_cname_record.rsr.zone_name}"
-    keyVaultId       = "${azurerm_key_vault.vault.id}"
-    keyVaultCertName = "${replace("${azurerm_dns_cname_record.rsr.name}.${azurerm_dns_cname_record.rsr.zone_name}", ".", "DOT")}"
-    service          = "${var.tags["Service"]}"
-    environment      = "${var.tags["Environment"]}"
+    keyVaultId       = azurerm_key_vault.vault.id
+    keyVaultCertName = replace("${azurerm_dns_cname_record.rsr.name}.${azurerm_dns_cname_record.rsr.zone_name}", ".", "DOT")
+    service          = var.tags["Service"]
+    environment      = var.tags["Environment"]
   }
 
-  depends_on = ["azurerm_app_service.app"]
+  depends_on = [azurerm_app_service.app]
 }
 
 module "slackhook-rsr" {
   source             = "../../shared/modules/slackhook"
-  app_name           = "${azurerm_app_service.app.name}"
+  app_name           = azurerm_app_service.app.name
   channels           = ["api-accelerator", "shef_changes"]
   azure_subscription = "production"
 }
