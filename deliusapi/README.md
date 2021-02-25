c8d9383f8dff9101092a999ee17affbbc054b8ab
Ynyr Llewelyn Williams <ynyr.williams@digital.justice.gov.uk>
2021-02-25 10:14:23 +0000

    DSO-768 - removed deliusapi and monitoring

--- deliusapi/README.md
+++
@@ -1,2 +0,0 @@
-#NOT USED
-will be deleted in the next merge once I figure out how to get a `terraform destroy` running
--- monitoring/prod/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "monitoring_ip" {
-  value = module.monitoring.monitoring_ip
-}
-
-output "monitoring_dns" {
-  value = module.monitoring.monitoring_dns_urls
-}
