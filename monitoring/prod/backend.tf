66b614cc03b33eb414aefef6fac2c944aafbb120
Ynyr Llewelyn Williams <ynyr.williams@digital.justice.gov.uk>
2021-02-25 10:14:23 +0000

    DSO-768 - removed deliusapi and monitoring

--- monitoring/prod/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "monitoring-prod"
-    storage_account_name = "monitoringprodstorage"
-    subscription_id      = "a5ddf257-3b21-4ba9-a28c-ab30f751b383"
-    container_name       = "terraform"
-    key                  = "monitoring-prod.terraform.tfstate"
-  }
-}
